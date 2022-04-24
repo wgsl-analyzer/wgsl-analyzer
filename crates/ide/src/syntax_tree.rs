66bbfe8b0be1b5c6a4d0a8a61e1d8f3d5b9e1000
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-04-24 22:27:59 +0800

    feat: 🎸 clean up, and finish range syntax tree

--- crates/ide/src/syntax_tree.rs
+++ crates/ide/src/syntax_tree.rs
@@ -11,9 +11,6 @@ pub fn syntax_tree(
     range: Option<Range>,
 ) -> Option<String> {
     let syntax_node = db.parse(file_id).syntax();
-    // let file_id = file_id(snap, &text_document_identifier.uri)?;
-    // let line_index = snap.file_line_index(file_id)?;
-    // let range = text_range(&line_index, range)?;
     if let Some(range) = range {
         let Range { start, end } = range;
         let line_index = db.line_index(file_id);
@@ -25,28 +22,12 @@ pub fn syntax_tree(
             line: end.line,
             col: end.character,
         })?;
-        let token_or_node = syntax_node.child_or_token_at_range(TextRange::new(start, end))?;
+        let token_or_node = syntax_node.covering_element(TextRange::new(start, end));
         match token_or_node {
             rowan::NodeOrToken::Node(node) => Some(format!("{:#?}", node)),
-            rowan::NodeOrToken::Token(token) => Some(format!("{:#?}", token.parent())),
+            rowan::NodeOrToken::Token(token) => Some(format!("{:#?}", token.parent()?)),
         }
     } else {
         Some(format!("{:#?}", syntax_node))
     }
 }
-// pub fn offset(line_index: &LineIndex, position: lsp_types::Position) -> Result<TextSize> {
-//     let line_col = match line_index.encoding {
-//         OffsetEncoding::Utf8 => LineCol {
-//             line: position.line as u32,
-//             col: position.character as u32,
-//         },
-//         OffsetEncoding::Utf16 => {
-//             let line_col = LineColUtf16 {
-//                 line: position.line as u32,
-//                 col: position.character as u32,
-//             };
-//             line_index.index.to_utf8(line_col)
-//         }
-//     };
-//     Ok(text_size)
-// }
