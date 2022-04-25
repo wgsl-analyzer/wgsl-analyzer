f73cbb1893f5c6484a82c8d894f65e7a0b15fbc6
IWANABETHATGUY <iwanabethatguy@qq.com>
2022-04-25 22:51:15 +0800

    fix: 🐛 fix CR issue, refactor code

--- crates/ide/src/syntax_tree.rs
+++ crates/ide/src/syntax_tree.rs
@@ -1,28 +1,16 @@
 use base_db::{
-    line_index::{LineCol, LineColUtf16, LineIndex},
     SourceDatabase, TextRange,
 };
-use lsp_types::Range;
 use vfs::FileId;
 
 pub fn syntax_tree(
     db: &dyn SourceDatabase,
     file_id: FileId,
-    range: Option<Range>,
+    range: Option<TextRange>,
 ) -> Option<String> {
     let syntax_node = db.parse(file_id).syntax();
     if let Some(range) = range {
-        let Range { start, end } = range;
-        let line_index = db.line_index(file_id);
-        let start = line_index.offset(LineCol {
-            line: start.line,
-            col: start.character,
-        })?;
-        let end = line_index.offset(LineCol {
-            line: end.line,
-            col: end.character,
-        })?;
-        let token_or_node = syntax_node.covering_element(TextRange::new(start, end));
+        let token_or_node = syntax_node.covering_element(range);
         match token_or_node {
             rowan::NodeOrToken::Node(node) => Some(format!("{:#?}", node)),
             rowan::NodeOrToken::Token(token) => Some(format!("{:#?}", token.parent()?)),
