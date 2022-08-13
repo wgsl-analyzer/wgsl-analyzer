9d86f81ee19dacf82d18cf32c41e47e571840fb4
Jakob Hellermann <jakob.hellermann@protonmail.com>
2022-08-13 11:19:37 +0200

    update versions

--- crates/wgsl_analyzer/src/bin/main.rs
+++ crates/wgsl_analyzer/src/bin/main.rs
@@ -8,7 +8,7 @@ use wgsl_analyzer::{
     Result,
 };
 
-const VERSION: &str = "0.4.6";
+const VERSION: &str = "0.5.0";
 
 fn main() -> Result<()> {
     if std::env::args().any(|arg| arg == "--version") {
--- editors/code/src/main.ts
+++ editors/code/src/main.ts
@@ -6,8 +6,8 @@ import * as fs from "fs";
 import * as util from "util";
 import * as cp from "child_process";
 
-const VERSION = "0.4.6";
-const REV = "56e61d1";
+const VERSION = "0.5.0";
+const REV = "a992070";
 
 const fileExists = (path: string) => util.promisify(fs.access)(path).then(s => true).catch(() => false);
 const exec = util.promisify(cp.exec);
