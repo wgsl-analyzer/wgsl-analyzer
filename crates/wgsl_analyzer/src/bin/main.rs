ce337646bb796c6bf24548054755ebcc615060be
Jakob Hellermann <jakob.hellermann@protonmail.com>
2022-10-29 00:00:35 +0200

    v0.5.3

--- crates/wgsl_analyzer/src/bin/main.rs
+++ crates/wgsl_analyzer/src/bin/main.rs
@@ -8,7 +8,7 @@ use wgsl_analyzer::{
     Result,
 };
 
-const VERSION: &str = "0.5.2";
+const VERSION: &str = "0.5.3";
 
 fn main() -> Result<()> {
     if std::env::args().any(|arg| arg == "--version") {
