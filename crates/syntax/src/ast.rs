392bc67f3037207ed7087f0fb8c468a30881c059
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2023-02-23 19:00:39 +0000

    Support Xor properly

--- crates/syntax/src/ast.rs
+++ crates/syntax/src/ast.rs
@@ -443,6 +443,7 @@ ast_token_enum! {
         AndAnd,
         Star,
         ForwardSlash,
+        Xor,
     }
 }
 ast_token_enum! {
@@ -902,6 +903,7 @@ impl InfixExpr {
                 BinaryOpKind::Modulo(_) => BinaryOp::ArithOp(ArithOp::Modulo),
                 BinaryOpKind::Or(_) => BinaryOp::ArithOp(ArithOp::BitOr),
                 BinaryOpKind::And(_) => BinaryOp::ArithOp(ArithOp::BitAnd),
+                BinaryOpKind::Xor(_)=>BinaryOp::ArithOp(ArithOp::BitXor),
                 BinaryOpKind::OrOr(_) => BinaryOp::LogicOp(LogicOp::Or),
                 BinaryOpKind::AndAnd(_) => BinaryOp::LogicOp(LogicOp::And),
             };
