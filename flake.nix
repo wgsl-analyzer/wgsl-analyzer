07acdbeda905d45a6bb1651ae5987d6e54a465b4
Oliver Sargison <sargo@sargo.cc>
2023-07-15 09:39:49 +1200

    Added nix flake
    Added a flake.nix file for use with nix enabled systems and projects. It allows the project to be built easily and reproducibly.
    
    Redid opts
    
    Build all packages
    
    rm name

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, naersk }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ rust-overlay.overlays.default ];
+        rust = pkgs.rust-bin.stable.latest.default;
+        builder = pkgs.callPackage naersk { rustc = rust; };
+        pkgs = import nixpkgs { inherit system overlays; };
+
+      in rec {
+        packages.default = builder.buildPackage {
+          name = "wgsl-analyzer";
+          src = ./.;
+          cargoBuildOptions = opts: ["-p" "wgsl_analyzer"] ++ opts;
+        };
+        apps.default = packages.default;
+        devShell =
+          pkgs.mkShell { packages = with pkgs; [ rust-analyzer rust ]; };
+      });
+}
