bfd71246ff13f3a264d1bf0b44224d1cfa2f5f50
Oliver Sargison <sargo@sargo.cc>
2023-11-13 12:47:31 +1300

    Add overlay and format

--- flake.nix
+++ flake.nix
@@ -10,25 +10,23 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         craneLib = crane.lib.${system};
-          wgslFilter = path: _type: builtins.match ".*wgsl$" path != null;
-          wgslOrCargo = path: type:
-            (wgslFilter path type) || (craneLib.filterCargoSources path type);
-      in
-    {
-      packages.default = craneLib.buildPackage {
-        src = nixpkgs.lib.cleanSourceWith {
-          src = craneLib.path ./.; 
-          filter = wgslOrCargo;
+        wgslFilter = path: _type: builtins.match ".*wgsl$" path != null;
+        wgslOrCargo = path: type:
+          (wgslFilter path type) || (craneLib.filterCargoSources path type);
+
+        wgsl_analyzer = craneLib.buildPackage {
+          src = nixpkgs.lib.cleanSourceWith {
+            src = craneLib.path ./.;
+            filter = wgslOrCargo;
+          };
+
+          cargoExtraArgs = "-p wgsl_analyzer";
+          pname = "wgsl_analyzer";
+          version = "0.0.0";
         };
-  
-        cargoExtraArgs = "-p wgsl_analyzer";
-        pname = "wgsl_analyzer";
-        version = "0.0.0";
-      };
-    });
+      in {
+        packages.default = wgsl_analyzer;
+        overlays.default = (self: super: { wgsl-analyzer = wgsl_analyzer; });
+      });
 }
-
-
-
-
 
