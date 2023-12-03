898fb4eaa3c766ed81f88e0727dae47a05b4fd2d
Sander <hey@sandydoo.me>
2023-12-03 23:57:56 +0400

    Fix nix builds for darwin

--- flake.nix
+++ flake.nix
@@ -9,16 +9,23 @@
   outputs = { nixpkgs, crane, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (pkgs) lib stdenv;
         craneLib = crane.lib.${system};
         wgslFilter = path: _type: builtins.match ".*wgsl$" path != null;
         wgslOrCargo = path: type:
           (wgslFilter path type) || (craneLib.filterCargoSources path type);
 
         wgsl_analyzer = craneLib.buildPackage {
-          src = nixpkgs.lib.cleanSourceWith {
+          src = lib.cleanSourceWith {
             src = craneLib.path ./.;
             filter = wgslOrCargo;
           };
+
+          buildInputs =
+            lib.optionals stdenv.isDarwin [
+              pkgs.libiconv
+            ];
 
           cargoExtraArgs = "-p wgsl_analyzer";
           pname = "wgsl_analyzer";
