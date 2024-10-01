c5d7b3aefd1b8c6d0fe4d22b806cc9132a7fe6a5
Al McElrath <hello@yrns.org>
2024-10-01 13:23:57 -0700

    Update flake.

--- flake.nix
+++ flake.nix
@@ -2,16 +2,21 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     crane.url = "github:ipetkov/crane";
-    crane.inputs.nixpkgs.follows = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, crane, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
+  outputs = {
+    self,
+    nixpkgs,
+    crane,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
         pkgs = nixpkgs.legacyPackages.${system};
         inherit (pkgs) lib stdenv;
-        craneLib = crane.lib.${system};
+        craneLib = crane.mkLib pkgs;
         wgslFilter = path: _type: builtins.match ".*wgsl$" path != null;
         wgslOrCargo = path: type:
           (wgslFilter path type) || (craneLib.filterCargoSources path type);
@@ -33,7 +38,9 @@
         };
       in {
         packages.default = wgsl_analyzer;
-        overlays.default = (self: super: { wgsl-analyzer = wgsl_analyzer; });
-      });
+      }
+    )
+    // {
+      overlays.default = final: prev: {wgsl-analyzer = self.packages.${final.system}.default;};
+    };
 }
-
