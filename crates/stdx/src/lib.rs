93769bf80101574250a141e08d63104fbbfbb542
BenjaminBrienen <Benjamin.Brienen@outlook.com>
2025-02-23 20:04:49 +0100

    add stdx

---
+++ crates/stdx/src/lib.rs
@@ -0,0 +1,404 @@
+//! Missing batteries for standard libraries.
+
+use std::{
+    cmp::Ordering,
+    hash::{BuildHasher, BuildHasherDefault},
+    ops,
+    time::Instant,
+};
+use std::{hash::Hash, io as sio};
+use std::{hash::Hasher, process::Command};
+
+pub mod anymap;
+mod macros;
+pub mod non_empty_vec;
+pub mod panic_context;
+pub mod process;
+pub mod thin_vec;
+pub mod thread;
+
+pub use always_assert::{always, never};
+pub use itertools;
+
+#[expect(clippy::inline_always, reason = "copy pasted from r-a")]
+#[inline(always)]
+#[must_use]
+pub const fn is_ci() -> bool {
+    option_env!("CI").is_some()
+}
+
+#[inline]
+pub fn hash_once<AHasher: Hasher + Default, Hashable: Hash>(thing: Hashable) -> u64 {
+    BuildHasher::hash_one(&BuildHasherDefault::<AHasher>::default(), thing)
+}
+
+#[must_use]
+#[inline]
+#[expect(clippy::print_stderr, reason = "copy pasted from r-a")]
+pub fn timeit(label: &'static str) -> impl Drop {
+    let start = Instant::now();
+    defer(move || eprintln!("{}: {:.2}", label, start.elapsed().as_nanos()))
+}
+
+/// Prints backtrace to stderr, useful for debugging.
+#[expect(clippy::print_stderr, reason = "copy pasted from r-a")]
+#[inline]
+pub fn print_backtrace() {
+    #[cfg(feature = "backtrace")]
+    #[expect(clippy::use_debug, reason = "Backtrace does not implement Display")]
+    {
+        eprintln!("{:?}", backtrace::Backtrace::new());
+    }
+
+    #[cfg(not(feature = "backtrace"))]
+    eprintln!(
+        r#"Enable the backtrace feature.
+Uncomment `default = [ "backtrace" ]` in `crates/stdx/Cargo.toml`.
+"#
+    );
+}
+
+pub trait TupleExt {
+    type Head;
+    type Tail;
+    fn head(self) -> Self::Head;
+    fn tail(self) -> Self::Tail;
+}
+
+impl<T, U> TupleExt for (T, U) {
+    type Head = T;
+    type Tail = U;
+
+    #[inline]
+    fn head(self) -> Self::Head {
+        self.0
+    }
+
+    #[inline]
+    fn tail(self) -> Self::Tail {
+        self.1
+    }
+}
+
+impl<T, U, V> TupleExt for (T, U, V) {
+    type Head = T;
+    type Tail = V;
+
+    #[inline]
+    fn head(self) -> Self::Head {
+        self.0
+    }
+
+    #[inline]
+    fn tail(self) -> Self::Tail {
+        self.2
+    }
+}
+
+#[inline]
+pub fn to_lower_snake_case(string: &str) -> String {
+    to_snake_case(string, char::to_lowercase)
+}
+
+#[inline]
+pub fn to_upper_snake_case(string: &str) -> String {
+    to_snake_case(string, char::to_uppercase)
+}
+
+// Code partially taken from rust/compiler/rustc_lint/src/nonstandard_style.rs
+// commit: 9626f2b
+fn to_snake_case<F, I>(
+    mut string: &str,
+    change_case: F,
+) -> String
+where
+    F: Fn(char) -> I,
+    I: Iterator<Item = char>,
+{
+    let mut words = vec![];
+
+    // Preserve leading underscores
+    string = string.trim_start_matches(|char: char| {
+        if char == '_' {
+            words.push(String::new());
+            true
+        } else {
+            false
+        }
+    });
+
+    for split in string.split('_') {
+        let mut last_upper = false;
+        let mut buf = String::new();
+
+        if split.is_empty() {
+            continue;
+        }
+
+        for ch in split.chars() {
+            if !buf.is_empty() && buf != "'" && ch.is_uppercase() && !last_upper {
+                words.push(buf);
+                buf = String::new();
+            }
+
+            last_upper = ch.is_uppercase();
+            buf.extend(change_case(ch));
+        }
+
+        words.push(buf);
+    }
+
+    words.join("_")
+}
+
+// Taken from rustc.
+#[inline]
+#[must_use]
+pub fn to_camel_case(ident: &str) -> String {
+    ident
+        .trim_matches('_')
+        .split('_')
+        .filter(|component| !component.is_empty())
+        .map(|component| {
+            let mut camel_cased_component = String::with_capacity(component.len());
+
+            let mut new_word = true;
+            let mut prev_is_lower_case = true;
+
+            for character in component.chars() {
+                // Preserve the case if an uppercase letter follows a lowercase letter, so that
+                // `camelCase` is converted to `CamelCase`.
+                if prev_is_lower_case && character.is_uppercase() {
+                    new_word = true;
+                }
+
+                if new_word {
+                    camel_cased_component.extend(character.to_uppercase());
+                } else {
+                    camel_cased_component.extend(character.to_lowercase());
+                }
+
+                prev_is_lower_case = character.is_lowercase();
+                new_word = false;
+            }
+
+            camel_cased_component
+        })
+        .fold(
+            (String::new(), None),
+            |(mut acc, prev): (_, Option<String>), next| {
+                // separate two components with an underscore if their boundary cannot
+                // be distinguished using an uppercase/lowercase case distinction
+                let join = prev
+                    .and_then(|prev| {
+                        let first = next.chars().next()?;
+                        let last = prev.chars().last()?;
+                        Some(!char_has_case(last) && !char_has_case(first))
+                    })
+                    .unwrap_or(false);
+                acc.push_str(if join { "_" } else { "" });
+                acc.push_str(&next);
+                (acc, Some(next))
+            },
+        )
+        .0
+}
+
+// Taken from rustc.
+#[inline]
+#[must_use]
+pub const fn char_has_case(char: char) -> bool {
+    char.is_lowercase() || char.is_uppercase()
+}
+
+#[inline]
+#[must_use]
+pub fn is_upper_snake_case(string: &str) -> bool {
+    string
+        .chars()
+        .all(|char| char.is_uppercase() || char == '_' || char.is_numeric())
+}
+
+#[inline]
+pub fn replace(
+    buf: &mut String,
+    from: char,
+    to: &str,
+) {
+    if !buf.contains(from) {
+        return;
+    }
+    // FIXME: do this in place.
+    *buf = buf.replace(from, to);
+}
+
+#[inline]
+#[must_use]
+pub fn trim_indent(mut text: &str) -> String {
+    if text.starts_with('\n') {
+        text = &text[1..];
+    }
+    let indent = text
+        .lines()
+        .filter(|it| !it.trim().is_empty())
+        .map(|it| it.len() - it.trim_start().len())
+        .min()
+        .unwrap_or(0);
+    text.split_inclusive('\n')
+        .map(|line| {
+            if line.len() <= indent {
+                line.trim_start_matches(' ')
+            } else {
+                &line[indent..]
+            }
+        })
+        .collect()
+}
+
+#[inline]
+pub fn equal_range_by<T, F>(
+    slice: &[T],
+    mut key: F,
+) -> ops::Range<usize>
+where
+    F: FnMut(&T) -> Ordering,
+{
+    let start = slice.partition_point(|it| key(it) == Ordering::Less);
+    let len = slice[start..].partition_point(|it| key(it) == Ordering::Equal);
+    start..start + len
+}
+
+#[must_use]
+#[inline]
+pub fn defer<Function: FnOnce()>(function: Function) -> impl Drop {
+    struct Droppable<Function: FnOnce()>(Option<Function>);
+
+    impl<F: FnOnce()> Drop for Droppable<F> {
+        fn drop(&mut self) {
+            if let Some(function) = self.0.take() {
+                function();
+            }
+        }
+    }
+    Droppable(Some(function))
+}
+
+/// A [`std::process::Child`] wrapper that will kill the child on drop.
+#[cfg_attr(not(target_arch = "wasm32"), repr(transparent))]
+#[derive(Debug)]
+pub struct JodChild(pub std::process::Child);
+
+impl ops::Deref for JodChild {
+    type Target = std::process::Child;
+    #[inline]
+    fn deref(&self) -> &std::process::Child {
+        &self.0
+    }
+}
+
+impl ops::DerefMut for JodChild {
+    #[inline]
+    fn deref_mut(&mut self) -> &mut std::process::Child {
+        &mut self.0
+    }
+}
+
+impl Drop for JodChild {
+    #[inline]
+    fn drop(&mut self) {
+        self.0.kill();
+        self.0.wait();
+    }
+}
+
+impl JodChild {
+    #[inline]
+    pub fn spawn(mut command: Command) -> sio::Result<Self> {
+        command.spawn().map(Self)
+    }
+
+    #[must_use]
+    #[inline]
+    #[cfg(target_arch = "wasm32")]
+    pub fn into_inner(self) -> std::process::Child {
+        // SAFETY: repr transparent, except on WASM
+        unsafe { std::mem::transmute::<Self, std::process::Child>(self) }
+    }
+}
+
+// feature: iter_order_by
+// Iterator::eq_by
+#[inline]
+pub fn iter_eq_by<I, I2, F>(
+    this: I2,
+    other: I,
+    mut eq: F,
+) -> bool
+where
+    I: IntoIterator,
+    I2: IntoIterator,
+    F: FnMut(I2::Item, I::Item) -> bool,
+{
+    let mut other = other.into_iter();
+    let mut this = this.into_iter();
+
+    loop {
+        let x = match this.next() {
+            None => return other.next().is_none(),
+            Some(val) => val,
+        };
+
+        let y = match other.next() {
+            None => return false,
+            Some(val) => val,
+        };
+
+        if !eq(x, y) {
+            return false;
+        }
+    }
+}
+
+/// Returns all final segments of the argument, longest first.
+#[inline]
+pub fn slice_tails<T>(this: &[T]) -> impl Iterator<Item = &[T]> {
+    (0..this.len()).map(|i| &this[i..])
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_trim_indent() {
+        assert_eq!(trim_indent(""), "");
+        assert_eq!(
+            trim_indent(
+                "
+            hello
+            world
+"
+            ),
+            "hello\nworld\n"
+        );
+        assert_eq!(
+            trim_indent(
+                "
+            hello
+            world"
+            ),
+            "hello\nworld"
+        );
+        assert_eq!(trim_indent("    hello\n    world\n"), "hello\nworld\n");
+        assert_eq!(
+            trim_indent(
+                "
+            fn main() {
+                return 92;
+            }
+        "
+            ),
+            "fn main() {\n    return 92;\n}\n"
+        );
+    }
+}
---
+++ crates/stdx/src/thread/pool.rs
@@ -0,0 +1,125 @@
+//! [`Pool`] implements a basic custom thread pool
+//! inspired by the [`threadpool` crate](http://docs.rs/threadpool).
+//! When you spawn a task you specify a thread intent
+//! so the pool can schedule it to run on a thread with that intent.
+//! wgsl-analyzer uses this to prioritize work based on latency requirements.
+//!
+//! The thread pool is implemented entirely using
+//! the threading utilities in [`crate::thread`].
+
+use std::{
+    panic::{self, UnwindSafe},
+    sync::{
+        Arc,
+        atomic::{AtomicUsize, Ordering},
+    },
+};
+
+use crossbeam_channel::{Receiver, Sender};
+
+use super::{Builder, JoinHandle, ThreadIntent};
+
+pub struct Pool {
+    // `_handles` is never read: the field is present
+    // only for its `Drop` impl.
+
+    // The worker threads exit once the channel closes;
+    // make sure to keep `job_sender` above `handles`
+    // so that the channel is actually closed
+    // before we join the worker threads!
+    job_sender: Sender<Job>,
+    _handles: Box<[JoinHandle]>,
+    extant_tasks: Arc<AtomicUsize>,
+}
+
+struct Job {
+    requested_intent: ThreadIntent,
+    function: Box<dyn FnOnce() + Send + UnwindSafe + 'static>,
+}
+
+impl Pool {
+    /// # Panics
+    ///
+    /// Panics if job panics
+    #[must_use]
+    #[inline]
+    pub fn new(threads: usize) -> Self {
+        const STACK_SIZE: usize = 8 * 1024 * 1024;
+        const INITIAL_INTENT: ThreadIntent = ThreadIntent::Worker;
+
+        let (job_sender, job_receiver) = crossbeam_channel::unbounded();
+        let extant_tasks = Arc::new(AtomicUsize::new(0));
+
+        let mut handles = Vec::with_capacity(threads);
+        for _ in 0..threads {
+            let handle = Builder::new(INITIAL_INTENT)
+                .stack_size(STACK_SIZE)
+                .name("Worker".into())
+                .allow_leak(true)
+                .spawn({
+                    let extant_tasks = Arc::clone(&extant_tasks);
+                    let job_receiver: Receiver<Job> = job_receiver.clone();
+                    move || {
+                        let mut current_intent = INITIAL_INTENT;
+                        for job in job_receiver {
+                            if job.requested_intent != current_intent {
+                                job.requested_intent.apply_to_current_thread();
+                                current_intent = job.requested_intent;
+                            }
+                            extant_tasks.fetch_add(1, Ordering::SeqCst);
+                            // discard the panic, we should have logged the backtrace already
+                            drop(panic::catch_unwind(job.function));
+                            extant_tasks.fetch_sub(1, Ordering::SeqCst);
+                        }
+                    }
+                })
+                .expect("failed to spawn thread");
+
+            handles.push(handle);
+        }
+
+        Self {
+            _handles: handles.into_boxed_slice(),
+            extant_tasks,
+            job_sender,
+        }
+    }
+
+    /// # Panics
+    ///
+    /// Panics if job panics
+    #[inline]
+    pub fn spawn<Function>(
+        &self,
+        intent: ThreadIntent,
+        function: Function,
+    ) where
+        Function: FnOnce() + Send + UnwindSafe + 'static,
+    {
+        #[expect(clippy::semicolon_if_nothing_returned, reason = "thin wrapper")]
+        let boxed_function = Box::new(move || {
+            if cfg!(debug_assertions) {
+                intent.assert_is_used_on_current_thread();
+            }
+            function()
+        });
+
+        let job = Job {
+            requested_intent: intent,
+            function: boxed_function,
+        };
+        self.job_sender.send(job).unwrap();
+    }
+
+    #[must_use]
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.extant_tasks.load(Ordering::SeqCst)
+    }
+
+    #[must_use]
+    #[inline]
+    pub fn is_empty(&self) -> bool {
+        self.extant_tasks.load(Ordering::SeqCst) == 0
+    }
+}
