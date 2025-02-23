9697a88a9834312cdcd44ba97354293742ec8d60
BenjaminBrienen <Benjamin.Brienen@outlook.com>
2025-02-23 20:47:37 +0100

    2021

--- crates/stdx/src/anymap.rs
+++ crates/stdx/src/anymap.rs
@@ -68,7 +68,6 @@ use ::std::collections::hash_map;
 /// Raw access to the underlying `HashMap`.
 ///
 /// This alias is provided for convenience because of the ugly third generic parameter.
-#[expect(clippy::disallowed_types, reason = "Uses a custom hasher")]
 pub type RawMap<A> = hash_map::HashMap<TypeId, Box<A>, BuildHasherDefault<TypeIdHasher>>;
 
 /// A collection containing zero or one values for any given type and allowing convenient,
