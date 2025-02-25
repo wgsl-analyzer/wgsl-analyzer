wgsl-analyzer/wgsl-analyzer

commit 4c56b1435d30cd45d8aee52297bbf68ed5bb3beb

rust 2024 (#176)
# Objective

update to rust 2024

## Solution

fix new lints, formatting, etc.

## Testing

CI

flake.nix
@@ -32,12 +32,12 @@
               pkgs.libiconv
             ];
 
-          cargoExtraArgs = "-p wgsl_analyzer";
-          pname = "wgsl_analyzer";
+          cargoExtraArgs = "-p wgsl-analyzer";
+          pname = "wgsl-analyzer";
           version = "0.0.0";
         };
       in {
-        packages.default = wgsl_analyzer;
+        packages.default = wgsl-analyzer;
       }
     )
     // {

commit 7efa510403694ba7bac32b7e5fd0d5e88c8b6d23

Fix flake package variable (#180)
# Objective

Nix flake is broken by
https://github.com/wgsl-analyzer/wgsl-analyzer/pull/176 as it references
an undefined variable.

If you are fixing a specific issue, simply say "Fixes #X".

## Solution

Set the correct variable name.

## Testing

- Did you test these changes? If so, how?
- Are there any parts that need more testing?
- How can other people (reviewers) test your changes? Is there anything
specific they need to know?
- If relevant, what platforms did you test these changes on, and are
there any important ones you cannot test?

## Showcase

This section is optional. If this PR does not include a visual change or
does not add a new feature, you can delete this section.

- Help others understand the result of this PR by showcasing your
awesome work!
- If this PR adds a new feature or public API, consider adding a brief
pseudo-code snippet of it in action
- If this PR includes a visual change, consider adding a screenshot,
GIF, or video
  - If you want, you could even include a before/after comparison!
- If the Migration Guide adequately covers the changes, you can delete
this section

While a showcase should aim to be brief and digestible, you can use a
toggleable section to save space on longer showcases:

```rust
println!("My super cool code.");
```

</details>

## Migration Guide

> This section is optional. If there are no breaking changes, you can
delete this section.

- If this PR is a breaking change (relative to the last release),
describe how a user might need to migrate their code to support these
changes
- Simply adding new functionality is not a breaking change.
- Fixing behavior that was definitely a bug, rather than a questionable
design choice is not a breaking change.

flake.nix
@@ -21,16 +21,15 @@
         wgslOrCargo = path: type:
           (wgslFilter path type) || (craneLib.filterCargoSources path type);
 
-        wgsl_analyzer = craneLib.buildPackage {
+        wgsl-analyzer = craneLib.buildPackage {
           src = lib.cleanSourceWith {
             src = craneLib.path ./.;
             filter = wgslOrCargo;
           };
 
-          buildInputs =
-            lib.optionals stdenv.isDarwin [
-              pkgs.libiconv
-            ];
+          buildInputs = lib.optionals stdenv.isDarwin [
+            pkgs.libiconv
+          ];
 
           cargoExtraArgs = "-p wgsl-analyzer";
           pname = "wgsl-analyzer";