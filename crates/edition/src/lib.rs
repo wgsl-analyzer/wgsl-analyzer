130dc5a5209c89e30efcca1b8c05abdea7851dfb
Benjamin Brienen <benjamin.brienen@outlook.com>
2025-04-03 18:25:44 +0200

    add edition crate (#243)
    # Objective
    
    Add a crate to provide an enum for "editions" of the language being
    analyzed.
    This can be expanded upon and used to support different versions of the
    WGSL specification (maybe once stability is a major concern) and also to
    support extension to the language such as WESL and extensions thereupon.
    
    This will also make it simpler to port and update other crates from r-a
    because there is something to fill that gap, even if it isn't very
    useful at the moment.
    
    The first crate that will use this is an update to `crates/parser`.
    
    `xtask` may also use it in the future in order to generate documentation
    about lints (after wgsl lints become a feature)
    
    Contributes to #182 
    
    ## Solution
    
    Port edition crate from r-a
    
    ## Testing
    
    N/A

---
+++ crates/edition/src/lib.rs
@@ -0,0 +1,92 @@
+//! The edition of the shader language.
+//! This should live here in a separate crate because we use it in both actual code and codegen.
+
+use std::fmt;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[repr(u8)]
+#[non_exhaustive]
+pub enum Edition {
+    // The syntax context stuff needs the discriminants to start from 0 and be consecutive.
+    Wgsl = 0,
+    Wesl0_0_1,
+}
+
+impl Edition {
+    pub const DEFAULT: Self = Self::Wgsl;
+    pub const LATEST: Self = Self::Wgsl;
+    pub const CURRENT: Self = Self::Wgsl;
+    /// The current latest stable edition, note this is usually not the right choice in code.
+    pub const CURRENT_FIXME: Self = Self::Wgsl;
+
+    /// # Panics
+    ///
+    /// Panics if the value does not correspond to a variant of [`Edition`].
+    #[must_use]
+    #[inline]
+    pub fn from_u32(u32: u32) -> Self {
+        match u32 {
+            0 => Self::Wgsl,
+            1 => Self::Wesl0_0_1,
+            _ => panic!("invalid edition"),
+        }
+    }
+
+    #[must_use]
+    #[inline]
+    pub fn at_least_wesl_0_0_1(self) -> bool {
+        self >= Self::Wesl0_0_1
+    }
+
+    #[inline]
+    pub fn iter() -> impl Iterator<Item = Self> {
+        [Self::Wgsl, Self::Wesl0_0_1].iter().copied()
+    }
+}
+
+#[derive(Debug)]
+pub struct ParseEditionError {
+    invalid_input: String,
+}
+
+impl std::error::Error for ParseEditionError {}
+
+impl fmt::Display for ParseEditionError {
+    #[inline]
+    fn fmt(
+        &self,
+        #[expect(clippy::min_ident_chars, reason = "trait impl")] f: &mut fmt::Formatter<'_>,
+    ) -> fmt::Result {
+        write!(f, "invalid edition: {}", self.invalid_input)
+    }
+}
+
+impl std::str::FromStr for Edition {
+    type Err = ParseEditionError;
+
+    #[inline]
+    fn from_str(
+        #[expect(clippy::min_ident_chars, reason = "trait impl")] s: &str
+    ) -> Result<Self, Self::Err> {
+        match s {
+            "WGSL" => Ok(Self::Wgsl),
+            "WESL 0.0.1" => Ok(Self::Wesl0_0_1),
+            _ => Err(ParseEditionError {
+                invalid_input: s.to_owned(),
+            }),
+        }
+    }
+}
+
+impl fmt::Display for Edition {
+    #[inline]
+    fn fmt(
+        &self,
+        #[expect(clippy::min_ident_chars, reason = "trait impl")] f: &mut fmt::Formatter<'_>,
+    ) -> fmt::Result {
+        f.write_str(match self {
+            Self::Wgsl => "WGSL",
+            Self::Wesl0_0_1 => "WESL 0.0.1",
+        })
+    }
+}
