d2855eb82ee5f69ceda6e89364b2d49546e306f9
Benjamin Brienen <benjamin.brienen@outlook.com>
2025-04-08 21:16:30 +0200

    remove errors crate (#261)
    remove unnecessary crate, was a bad idea for this project

--- errors/README.md
+++
@@ -1,3 +0,0 @@
-# Error Codes
-
-This crate lists and tests explanations and examples of the error codes.
--- errors/src/lib.rs
+++
@@ -1,4 +0,0 @@
-//! Definitions of `wgsl-analyzer`'s error codes that might occur at runtime.
-
-#[doc = include_str!("../T0001.md")]
-pub struct T0001;
