5785d1a37a42c7a0809fcaf1264308eed4d30433
BenjaminBrienen <Benjamin.Brienen@outlook.com>
2025-08-16 23:01:37 +0200

    add test-utils and add necessary output to codegen

--- xtask/src/codegen/assists_doc_tests.rs
+++ xtask/src/codegen/assists_doc_tests.rs
@@ -1,1 +1,16 @@
-pub(crate) fn generate(check: bool) {}
+use std::fs;
+
+use crate::{codegen::add_preamble, project_root};
+
+pub(crate) fn generate(check: bool) {
+    if check {
+        return;
+    }
+    // Generate assists manual.
+    // Note that we do *not* commit manual to the git repository.
+    // Instead, `cargo xtask release` runs this test before making a release.
+    let contents = String::new();
+    let contents = add_preamble(crate::flags::CodegenType::AssistsDocTests, contents);
+    let destination = project_root().join("docs/book/src/assists_generated.md");
+    fs::write(destination, contents).unwrap();
+}
--- xtask/src/codegen/lints.rs
+++ xtask/src/codegen/lints.rs
@@ -1,1 +1,15 @@
-pub(crate) fn generate(check: bool) {}
+use std::fs;
+
+use crate::{codegen::add_preamble, project_root};
+
+const DESTINATION: &str = "crates/ide-db/src/generated/lints.rs";
+pub(crate) fn generate(check: bool) {
+    // Do not generate assists manual when run with `--check`
+    if check {
+        return;
+    }
+    let contents = String::new();
+    let contents = add_preamble(crate::flags::CodegenType::LintDefinitions, contents);
+    let destination = project_root().join(DESTINATION);
+    fs::write(destination, contents).unwrap();
+}
