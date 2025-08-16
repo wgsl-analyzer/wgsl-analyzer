2bd11ffd9ac5b77ad55aca0ea4e5c13dbeb9f2c4
Mona Mayrhofer <mona.mayrhofer@proton.me>
2025-08-16 16:16:06 +0200

    Rename the tests to smoke_tests because thats what they are

--- crates/wgsl_formatter/src/tests.rs
+++ crates/wgsl_formatter/src/tests.rs
@@ -242,9 +242,9 @@ fn format_struct() {
 fn format_struct_body() {
     check(
         "
-        struct  Test   
-        {  @location(0) x: i32,                    a: i32,  
-        b: f32, 
+        struct  Test
+        {  @location(0) x: i32,                    a: i32,
+        b: f32,
 
                 }",
         expect![["
@@ -599,38 +599,38 @@ fn main() {
 }
 
 #[test]
-fn snapshot_tests() {
-    let source_path = Path::new("./tests/snapshots/source");
-    let output_path = Path::new("./tests/snapshots/output");
+fn smoke_tests() {
+    let source_path = Path::new("./tests/smoke_tests/source");
+    let output_path = Path::new("./tests/smoke_tests/output");
 
     //At this stage, the old formatter is not idempotent on all the tests.
     // But we want to achieve feature parity first, so we focus on the cases
     // where the old formatter works first, and generate tests that the new formatter works with.
     let skip = [OsString::from("old_formatter_idempotence.wgsl")];
 
-    for entry in std::fs::read_dir(source_path).expect("snapshot source directory should exist") {
-        let entry = entry.expect("snapshot source directory should be traversable");
-        let snapshot_source_path = entry.path();
-        if snapshot_source_path.is_file() {
-            let snapshot_name = snapshot_source_path
+    for entry in std::fs::read_dir(source_path).expect("smoke test source directory should exist") {
+        let entry = entry.expect("smoke test source directory should be traversable");
+        let smoke_test_source_path = entry.path();
+        if smoke_test_source_path.is_file() {
+            let smoke_test_name = smoke_test_source_path
                 .file_name()
-                .expect("snapshot source should be a normal file.");
+                .expect("smoke_test source should be a normal file.");
 
-            if skip.iter().any(|it| it == snapshot_name) {
+            if skip.iter().any(|it| it == smoke_test_name) {
                 continue;
             }
 
-            let snapshot_output_path = std::env::current_dir()
+            let smoke_test_output_path = std::env::current_dir()
                 .unwrap()
                 .join(output_path)
-                .join(snapshot_name);
+                .join(smoke_test_name);
 
-            let source = std::fs::read_to_string(snapshot_source_path)
+            let source = std::fs::read_to_string(smoke_test_source_path)
                 .expect("source file should be a readable text file.");
 
-            check(&source, expect_file![snapshot_output_path]);
+            check(&source, expect_file![smoke_test_output_path]);
         } else {
-            panic!("Expected snapshot directory to not have subdirectories.")
+            panic!("Expected smoke_test directory to not have subdirectories.")
         }
     }
 }
