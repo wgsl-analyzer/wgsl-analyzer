4a2c65ad0bdae0612835f292e6a94ddc5082ef8f
Mona Mayrhofer <mona.mayrhofer@proton.me>
2025-08-19 10:08:01 +0200

    Move out helpers

--- crates/wgsl_formatter/src/format.rs
+++ crates/wgsl_formatter/src/format.rs
@@ -1,3 +1,5 @@
+mod helpers;
+
 use std::{alloc::alloc, iter::repeat_with};
 
 use parser::SyntaxNode;
@@ -7,7 +9,7 @@ use syntax::{
     ast::{self},
 };
 
-use crate::FormattingOptions;
+use crate::{FormattingOptions, format::helpers::pretty_spaced_lines};
 
 #[must_use]
 pub fn format_str(
@@ -53,75 +55,6 @@ where
         ast::Item::TypeAliasDeclaration(type_alias_declaration) => todo!(),
         ast::Item::StructDeclaration(struct_declaration) => todo!(),
     }
-}
-
-/// Lays out the children of a node in a way so that
-/// *) after every node is exactly 1 or 2 newlines
-/// *) there are no newlines at the start of the file
-fn pretty_spaced_lines<'ann, D, TAnnotation>(
-    node: &parser::SyntaxNode,
-    allocator: &'ann D,
-    mut pretty_node: impl FnMut(SyntaxNode) -> DocBuilder<'ann, D, TAnnotation>,
-) -> DocBuilder<'ann, D, TAnnotation>
-where
-    D: DocAllocator<'ann, TAnnotation>,
-{
-    let mut result = allocator.nil();
-
-    enum NewLineState {
-        AtStartOfBlock,
-        NewLinesAfterNode(usize),
-    }
-
-    let mut new_line_state = NewLineState::AtStartOfBlock;
-
-    for child in node.children_with_tokens() {
-        match child {
-            rowan::NodeOrToken::Token(token) => {
-                if token.kind() == parser::SyntaxKind::Blankspace {
-                    //TODO Think a bit more about different types of newlines
-                    //TODO child.to_string() here surely is wasteful - there must be a better way.
-
-                    let newlines = token
-                        .to_string()
-                        .chars()
-                        .filter(|item| *item == '\n')
-                        .count();
-                    new_line_state = match new_line_state {
-                        //no newlines at start of block
-                        NewLineState::AtStartOfBlock => NewLineState::AtStartOfBlock,
-                        NewLineState::NewLinesAfterNode(count) => {
-                            NewLineState::NewLinesAfterNode(count + newlines)
-                        },
-                    };
-                }
-            },
-            rowan::NodeOrToken::Node(node) => {
-                match new_line_state {
-                    NewLineState::AtStartOfBlock => {},
-                    NewLineState::NewLinesAfterNode(count) => {
-                        result =
-                            result.append(allocator.concat(
-                                repeat_with(|| allocator.hardline()).take(count.clamp(1, 2)),
-                            ));
-                    },
-                }
-
-                result = result.append(pretty_node(node));
-                new_line_state = NewLineState::NewLinesAfterNode(0);
-            },
-        }
-    }
-
-    match new_line_state {
-        NewLineState::AtStartOfBlock => {},
-        NewLineState::NewLinesAfterNode(count) => {
-            //There should be a newline at the end of the file
-            result = result.append(allocator.hardline());
-        },
-    }
-
-    result
 }
 
 fn pretty_source_file<'ann, D, TAnnotation>(
