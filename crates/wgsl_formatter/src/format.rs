6b6f8e5865f4a103edef5008d96ce37200e7f7b9
Mona Mayrhofer <mona.mayrhofer@proton.me>
2025-08-21 16:50:33 +0200

    more exploratory work on fn signature layout

--- crates/wgsl_formatter/src/format.rs
+++ crates/wgsl_formatter/src/format.rs
@@ -3,16 +3,18 @@ mod reporting;
 
 use std::{alloc::alloc, iter::repeat_with};
 
-use parser::SyntaxNode;
+use parser::{SyntaxKind, SyntaxNode};
 use pretty::{BoxAllocator, DocAllocator, DocBuilder};
 use syntax::{
     AstNode as _, HasName as _,
     ast::{self},
+    match_ast,
 };
 
 use crate::{
     FormattingOptions,
     format::{
+        self,
         helpers::pretty_spaced_lines,
         reporting::{FormatDocumentError, FormatDocumentErrorKind, FormatDocumentResult},
     },
@@ -75,10 +77,7 @@ where
         if let Some(item) = ast::Item::cast(child.clone()) {
             pretty_item(&item, allocator)
         } else {
-            Err(FormatDocumentError {
-                error_kind: reporting::FormatDocumentErrorKind::UnexpectedModuleNode,
-                syntax_node: child,
-            })
+            Err(FormatDocumentErrorKind::UnexpectedModuleNode.at(child.text_range()))
         }
     })
 }
@@ -90,77 +89,242 @@ fn pretty_function_declaration<'ann, D, TAnnotation>(
 where
     D: DocAllocator<'ann, TAnnotation>,
 {
-    let name = node
-        .name()
-        .ok_or_else(|| FormatDocumentErrorKind::MissingFnName.at(node.syntax().clone()))?;
-    let name = name.text();
-    let node_params = node
-        .parameter_list()
-        .ok_or_else(|| FormatDocumentErrorKind::MissingFnParams.at(node.syntax().clone()))?
-        .parameters();
-
-    let formatted_params = node_params
-        .map(|param| {
-            let p_name = param.name().ok_or_else(|| {
-                FormatDocumentErrorKind::MissingFnParamName.at(param.syntax().clone())
-            })?;
-            let p_type = param.ty().ok_or_else(|| {
-                FormatDocumentErrorKind::MissingFnParamType.at(param.syntax().clone())
-            })?;
-            Ok((p_name, p_type))
-        })
-        .collect::<FormatDocumentResult<Vec<_>>>()?;
-
-    let return_type = node.return_type().and_then(|return_type| return_type.ty());
-
-    //TODO Don't to_owned here, but instead specify smarter lifetimes
-    let mut built_fn = allocator
-        .text("fn ")
-        .append(allocator.text(name.as_str().to_owned()))
-        .append(
-            allocator
-                .intersperse(
-                    formatted_params.iter().map(|(param_name, param_type)| {
-                        allocator
-                            .text(param_name.text().as_str().to_owned())
-                            .append(allocator.text(": "))
-                            .append(pretty_type_specifier(param_type, allocator))
-                    }),
-                    ", ",
-                )
-                .parens(),
-        );
-    if let Some(return_type) = return_type {
-        built_fn = built_fn
-            .append(allocator.text(" -> "))
-            .append(pretty_type_specifier(&return_type, allocator));
+    enum FunctionDeclarationState {
+        Init,
+        HasFn,
+        HasName,
+        HasParams,
+        HasReturnType,
+        HasBody,
     }
-    built_fn = built_fn
-        .append(allocator.text(" "))
-        .append(allocator.text("{}"));
-    Ok(built_fn)
+
+    let mut state = FunctionDeclarationState::Init;
+    let mut formatted = allocator.nil();
+
+    for child in node.syntax().children_with_tokens() {
+        match state {
+            FunctionDeclarationState::Init => {
+                if child.kind() == SyntaxKind::Blankspace {
+                    // Is allowed, we ignore it
+                } else if child.kind() == SyntaxKind::Fn {
+                    formatted = formatted
+                        .append(allocator.text("fn"))
+                        .append(allocator.space());
+                    state = FunctionDeclarationState::HasFn;
+                } else {
+                    return Err(FormatDocumentErrorKind::UnexpectedToken.at(child.text_range()));
+                }
+            },
+            FunctionDeclarationState::HasFn => {
+                if child.kind() == SyntaxKind::Blankspace {
+                    // Is allowed, we ignore it
+                } else if let Some(name) = child.as_node().cloned().and_then(ast::Name::cast) {
+                    formatted = formatted.append(allocator.text(name.text().to_string()));
+                    state = FunctionDeclarationState::HasName;
+                } else {
+                    return Err(FormatDocumentErrorKind::UnexpectedToken.at(child.text_range()));
+                }
+            },
+            FunctionDeclarationState::HasName => {
+                if child.kind() == SyntaxKind::Blankspace {
+                    // Is allowed, we ignore it
+                } else if let Some(params) = child
+                    .as_node()
+                    .cloned()
+                    .and_then(ast::FunctionParameters::cast)
+                {
+                    formatted = formatted.append(pretty_fn_parameters(&params, allocator)?);
+                    state = FunctionDeclarationState::HasParams;
+                } else {
+                    return Err(FormatDocumentErrorKind::UnexpectedToken.at(child.text_range()));
+                }
+            },
+            FunctionDeclarationState::HasParams => {
+                if child.kind() == SyntaxKind::Blankspace {
+                    // Is allowed, we ignore it
+                } else if let Some(return_type) =
+                    child.as_node().cloned().and_then(ast::ReturnType::cast)
+                {
+                    formatted = formatted
+                        .append(allocator.space()) //There is no case where there wouldn't be a space here.
+                        .append(pretty_fn_return_type(&return_type, allocator)?);
+                    state = FunctionDeclarationState::HasReturnType;
+                } else if let Some(body) = child
+                    .as_node()
+                    .cloned()
+                    .and_then(ast::CompoundStatement::cast)
+                {
+                    formatted = formatted
+                        .append(allocator.space()) //There is no case where there wouldn't be a space here.
+                        .append(pretty_fn_body(&body, allocator)?);
+                    state = FunctionDeclarationState::HasBody;
+                } else {
+                    return Err(FormatDocumentErrorKind::UnexpectedToken.at(child.text_range()));
+                }
+            },
+            FunctionDeclarationState::HasReturnType => {
+                //TODO This is duplication of the second HasParams clause, because the return type is optional
+                if child.kind() == SyntaxKind::Blankspace {
+                    // Is allowed, we ignore it
+                } else if let Some(body) = child
+                    .as_node()
+                    .cloned()
+                    .and_then(ast::CompoundStatement::cast)
+                {
+                    formatted = formatted
+                        .append(allocator.space()) //There is no case where there wouldn't be a space here.
+                        .append(pretty_fn_body(&body, allocator)?);
+                    state = FunctionDeclarationState::HasBody;
+                } else {
+                    return Err(FormatDocumentErrorKind::UnexpectedToken.at(child.text_range()));
+                }
+            },
+            FunctionDeclarationState::HasBody => {
+                return Err(FormatDocumentErrorKind::UnexpectedToken.at(child.text_range()));
+            },
+        }
+    }
+
+    if matches!(FunctionDeclarationState::HasBody, state) {
+        Ok(formatted)
+    } else {
+        Err(FormatDocumentErrorKind::MissingTokens.at(node.syntax().text_range()))
+    }
 }
 
 fn pretty_fn_parameters<'ann, D, TAnnotation>(
-    type_specifier: &ast::TypeSpecifier,
+    syntax: &ast::FunctionParameters,
     allocator: &'ann D,
-) -> DocBuilder<'ann, D, TAnnotation>
+) -> FormatDocumentResult<DocBuilder<'ann, D, TAnnotation>>
+where
+    D: DocAllocator<'ann, TAnnotation>,
+{
+    let mut formatted = allocator.line_();
+
+    let mut queued_comma = None;
+
+    for child in syntax.syntax().children_with_tokens() {
+        if child.kind() == SyntaxKind::Blankspace
+            || child.kind() == SyntaxKind::ParenthesisLeft
+            || child.kind() == SyntaxKind::ParenthesisRight
+        {
+            // Is allowed, we ignore it
+        } else if child.kind() == SyntaxKind::Comma {
+            queued_comma = Some(allocator.text(",").append(allocator.line()));
+        } else if let Some(parameter) = child.as_node().cloned().and_then(ast::Parameter::cast) {
+            formatted = formatted
+                .append(std::mem::take(&mut queued_comma))
+                .append(pretty_fn_parameter(&parameter, allocator)?);
+        } else {
+            return Err(FormatDocumentErrorKind::UnexpectedToken.at(child.text_range()));
+        }
+    }
+
+    formatted = formatted
+        .append(allocator.text(",").flat_alt(allocator.nil()))
+        .append(allocator.line_());
+
+    //Both states are fine
+    Ok(formatted.group().nest(4).parens())
+}
+
+fn pretty_fn_parameter<'ann, D, TAnnotation>(
+    syntax: &ast::Parameter,
+    allocator: &'ann D,
+) -> FormatDocumentResult<DocBuilder<'ann, D, TAnnotation>>
+where
+    D: DocAllocator<'ann, TAnnotation>,
+{
+    enum ParameterState {
+        Init,
+        HasName,
+        HasType,
+    }
+
+    let mut state = ParameterState::Init;
+    let mut formatted = allocator.nil();
+
+    for child in syntax.syntax().children_with_tokens() {
+        match state {
+            ParameterState::Init => {
+                if child.kind() == SyntaxKind::Blankspace
+                    || child.kind() == SyntaxKind::ParenthesisLeft
+                {
+                    // Is allowed, we ignore it
+                } else if let Some(name) = child.as_node().cloned().and_then(ast::Name::cast) {
+                    formatted = formatted
+                        .append(allocator.text(name.text().to_string()))
+                        .append(allocator.text(": "));
+                    state = ParameterState::HasName;
+                } else {
+                    return Err(FormatDocumentErrorKind::UnexpectedToken.at(child.text_range()));
+                }
+            },
+            ParameterState::HasName => {
+                if child.kind() == SyntaxKind::Blankspace || child.kind() == SyntaxKind::Colon {
+                    // Is allowed, we ignore it
+                } else if let Some(type_specifier) =
+                    child.as_node().cloned().and_then(ast::TypeSpecifier::cast)
+                {
+                    formatted =
+                        formatted.append(pretty_type_specifier(&type_specifier, allocator)?);
+                    state = ParameterState::HasType;
+                } else {
+                    return Err(FormatDocumentErrorKind::UnexpectedToken.at(child.text_range()));
+                }
+            },
+            ParameterState::HasType => {
+                //TODO This duplicates INIT state
+                if child.kind() == SyntaxKind::Blankspace || child.kind() == SyntaxKind::Comma {
+                    // Is allowed, we ignore it
+                } else if let Some(name) = child.as_node().cloned().and_then(ast::Name::cast) {
+                    formatted = formatted.append(allocator.text(name.text().to_string()));
+                    state = ParameterState::HasName;
+                } else {
+                    return Err(FormatDocumentErrorKind::UnexpectedToken.at(child.text_range()));
+                }
+            },
+        }
+    }
+
+    if matches!(ParameterState::HasType, state) {
+        Ok(formatted)
+    } else {
+        Err(FormatDocumentErrorKind::MissingTokens.at(syntax.syntax().text_range()))
+    }
+}
+
+fn pretty_fn_return_type<'ann, D, TAnnotation>(
+    syntax: &ast::ReturnType,
+    allocator: &'ann D,
+) -> FormatDocumentResult<DocBuilder<'ann, D, TAnnotation>>
 where
     D: DocAllocator<'ann, TAnnotation>,
 {
     //TODO
-    allocator.text(type_specifier.syntax().to_string())
+    todo_verbatim(syntax.syntax(), allocator)
+}
+
+fn pretty_fn_body<'ann, D, TAnnotation>(
+    syntax: &ast::CompoundStatement,
+    allocator: &'ann D,
+) -> FormatDocumentResult<DocBuilder<'ann, D, TAnnotation>>
+where
+    D: DocAllocator<'ann, TAnnotation>,
+{
+    //TODO
+    todo_verbatim(syntax.syntax(), allocator)
 }
 
 fn pretty_type_specifier<'ann, D, TAnnotation>(
     type_specifier: &ast::TypeSpecifier,
     allocator: &'ann D,
-) -> DocBuilder<'ann, D, TAnnotation>
+) -> FormatDocumentResult<DocBuilder<'ann, D, TAnnotation>>
 where
     D: DocAllocator<'ann, TAnnotation>,
 {
     //TODO
-    allocator.text(type_specifier.syntax().to_string())
+    todo_verbatim(type_specifier.syntax(), allocator)
 }
 
 /// In cases where the formatter is not yet complete we simply output source verbatim.
@@ -168,9 +332,9 @@ where
 fn todo_verbatim<'ann, D, TAnnotation>(
     source: &parser::SyntaxNode,
     allocator: &'ann D,
-) -> DocBuilder<'ann, D, TAnnotation>
+) -> FormatDocumentResult<DocBuilder<'ann, D, TAnnotation>>
 where
     D: DocAllocator<'ann, TAnnotation>,
 {
-    allocator.text(source.to_string())
+    Ok(allocator.text(source.to_string()))
 }
