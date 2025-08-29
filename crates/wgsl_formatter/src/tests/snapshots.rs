26ecf10da9a8888e7f2eb5282a4b8a62a324fd27
Mona Mayrhofer <mona.mayrhofer@proton.me>
2025-08-29 15:42:39 +0200

    Handle spacing via the PrintItemBuffer

--- crates/wgsl_formatter/src/tests/snapshots.rs
+++ crates/wgsl_formatter/src/tests/snapshots.rs
@@ -2,10 +2,15 @@ use expect_test::expect;
 
 use crate::test_util::{check, check_tabs};
 
+//TODO Remove this comment == MODULES THAT FULLY PASS ==
+mod bevy_reference;
+mod comments;
+
+//TODO Remove this comment == MODULES THAT STILL CONTAIN FAILING TESTS ==
 mod bindings;
 mod code_indentation;
 mod code_spacing;
-mod comments;
+mod common_conventions;
 mod control_structures;
 mod fn_call;
 mod fn_signature;
@@ -15,69 +20,4 @@ mod structs;
 #[test]
 fn format_empty() {
     check("", expect![[""]]);
-}
-
-#[test]
-fn format_bevy_function() {
-    check(
-        "fn directional_light(light: DirectionalLight, roughness: f32, NdotV: f32, normal: vec3<f32>, view: vec3<f32>, R: vec3<f32>, F0: vec3<f32>, diffuseColor: vec3<f32>) -> vec3<f32> {}",
-        expect![[
-            "fn directional_light(light: DirectionalLight, roughness: f32, NdotV: f32, normal: vec3<f32>, view: vec3<f32>, R: vec3<f32>, F0: vec3<f32>, diffuseColor: vec3<f32>) -> vec3<f32> {}"
-        ]],
-    );
-}
-
-#[test]
-fn format_bevy_function_2() {
-    check(
-        "fn specular(f0: vec3<f32>, roughness: f32, h: vec3<f32>, NoV: f32, NoL: f32,
-              NoH: f32, LoH: f32, specularIntensity: f32) -> vec3<f32> {",
-        expect![["
-                fn specular(f0: vec3<f32>, roughness: f32, h: vec3<f32>, NoV: f32, NoL: f32,
-                    NoH: f32, LoH: f32, specularIntensity: f32) -> vec3<f32> {"]],
-    );
-}
-
-#[test]
-fn leave_matrix_alone() {
-    check(
-        "
-fn main() {
-    let x = mat3x3(
-        cosR,  0.0, sinR,
-        0.0, 1.0, 0.0,
-        -sinR, 0.0, cosR,
-    );
-}",
-        expect![["
-            fn main() {
-                let x = mat3x3(
-                    cosR, 0.0, sinR,
-                    0.0, 1.0, 0.0,
-                    -sinR, 0.0, cosR,
-                );
-            }"]],
-    );
-}
-
-#[test]
-fn leave_matrix_alone_tabs() {
-    check_tabs(
-        "
-fn main() {
-	let x = mat3x3(
-		cosR,  0.0, sinR,
-		0.0, 1.0, 0.0,
-		-sinR, 0.0, cosR,
-	);
-}",
-        expect![["
-			fn main() {
-				let x = mat3x3(
-					cosR, 0.0, sinR,
-					0.0, 1.0, 0.0,
-					-sinR, 0.0, cosR,
-				);
-			}"]],
-    );
 }
