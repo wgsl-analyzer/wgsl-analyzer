730ff7121d067a2826cebb2be572c4da88f5034b
Mona Mayrhofer <mona.mayrhofer@proton.me>
2025-12-04 12:43:21 +0100

    Fix non Ident-Expression in index and field access

--- crates/wgsl_formatter/src/tests/snapshots/exprs_field.rs
+++ crates/wgsl_formatter/src/tests/snapshots/exprs_field.rs
@@ -34,6 +34,68 @@ pub fn format_field_expr_simple() {
 }
 
 #[test]
+pub fn format_field_nested_within_fields() {
+    check(
+        "fn main() {
+        let a =
+        foo
+        .
+        bar
+        .
+        baz
+        .
+        blub
+        .
+        blob
+        ;
+        }",
+        expect![["
+            fn main() {
+                let a = foo.bar.baz.blub.blob;
+            }
+        "]],
+    );
+}
+
+#[test]
+pub fn format_field_nested_within_indices() {
+    check(
+        "fn main() {
+        let a =
+        foo[17]
+        .
+        bar[28]
+        .
+        baz[39]
+        ;
+        }",
+        expect![["
+            fn main() {
+                let a = foo[17].bar[28].baz[39];
+            }
+        "]],
+    );
+}
+
+#[test]
+pub fn format_field_nested_within_various_things() {
+    check(
+        "fn main() {
+        let a =
+        get_my_foo()
+        .
+        bar
+        ;
+        }",
+        expect![["
+            fn main() {
+                let a = get_my_foo().bar;
+            }
+        "]],
+    );
+}
+
+#[test]
 pub fn format_comments_in_field_expr() {
     check_comments(
         "fn main() {
@@ -51,6 +113,31 @@ pub fn format_comments_in_field_expr() {
                     . // 2
                     bar // 3
                     ; // 4
+            }
+        "#]],
+    );
+}
+
+#[test]
+pub fn format_comments_in_nested_field_expr() {
+    check_comments(
+        "fn main() {
+        let a = ## foo ## . ## bar ## . ## baz ## ; ##
+        }",
+        expect![[r#"
+            fn main() {
+                let a = /* 0 */ foo /* 1 */ . /* 2 */ bar /* 3 */ . /* 4 */ baz /* 5 */; /* 6 */
+            }
+        "#]],
+        expect![[r#"
+            fn main() {
+                let a = // 0
+                    foo // 1
+                    . // 2
+                    bar // 3
+                    . // 4
+                    baz // 5
+                    ; // 6
             }
         "#]],
     );
