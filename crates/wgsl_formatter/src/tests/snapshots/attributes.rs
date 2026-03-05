37491f8bdc69f642feda28e68092a8bf80a509a0
Mona Mayrhofer <mona.mayrhofer@proton.me>
2026-03-05 12:13:53 +0100

    single attribute formatting

--- crates/wgsl_formatter/src/tests/snapshots/attributes.rs
+++ crates/wgsl_formatter/src/tests/snapshots/attributes.rs
@@ -1,6 +1,6 @@
 use expect_test::expect;
 
-use crate::test_util::check;
+use crate::test_util::{check, check_comments};
 
 #[test]
 pub fn format_attr_simple() {
@@ -29,7 +29,7 @@ pub fn format_attr_with_argument() {
         fn main() {}
         ",
         expect![[r#"
-            @fragment
+            @attr(0)
             fn main() {}
         "#]],
     );
@@ -49,7 +49,7 @@ pub fn format_attr_with_multiple_arguments() {
         fn main() {}
         ",
         expect![[r#"
-            @fragment
+            @attr(0, 0, 0)
             fn main() {}
         "#]],
     );
@@ -68,8 +68,89 @@ pub fn format_attr_with_text_arguments() {
         fn main() {}
         ",
         expect![[r#"
-            @fragment
+            @builtin(magic, thing)
             fn main() {}
+        "#]],
+    );
+}
+
+#[test]
+pub fn format_comments_in_attr_simple() {
+    check_comments(
+        "
+        ## @ ## fragment ## fn ## main() {}
+        ",
+        expect![[r#"
+            /* 0 */
+            @ /* 1 */ fragment /* 2 */
+            fn /* 3 */ main() {}
+        "#]],
+        expect![[r#"
+            // 0
+            @ // 1
+            fragment // 2
+            fn // 3
+            main() {}
+        "#]],
+    );
+}
+
+#[test]
+pub fn format_comments_in_attr_with_multiple_arguments() {
+    check_comments(
+        "
+        ## @ ## attr ## ( ## 0 ## , ## 0 ## , ## 0 ## ) ## fn ## main() {}
+        ",
+        expect![[r#"
+            /* 0 */
+            @ /* 1 */ attr /* 2 */ (
+                /* 3 */ 0, /* 4 */ /* 5 */
+                0, /* 6 */ /* 7 */
+                0, /* 8 */
+            ) /* 9 */
+            fn /* 10 */ main() {}
+        "#]],
+        expect![[r#"
+            // 0
+            @ // 1
+            attr // 2
+            (
+                // 3
+                0, // 4
+                // 5
+                0, // 6
+                // 7
+                0, // 8
+            ) // 9
+            fn // 10
+            main() {}
+        "#]],
+    );
+}
+
+#[test]
+pub fn format_comments_in_attr_with_text_arguments() {
+    check_comments(
+        "
+        ## @ ## builtin ## ( ## magic ## , ## thing ## ) ## fn ## main() {}
+        ",
+        expect![[r#"
+            /* 0 */
+            @ /* 1 */ builtin /* 2 */ (/* 3 */ magic, /* 4 */ /* 5 */ thing /* 6 */) /* 7 */
+            fn /* 8 */ main() {}
+        "#]],
+        expect![[r#"
+            // 0
+            @ // 1
+            builtin // 2
+            (
+                // 3
+                magic, // 4
+                // 5
+                thing, // 6
+            ) // 7
+            fn // 8
+            main() {}
         "#]],
     );
 }
