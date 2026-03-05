98f231c61b651593470d8ea9bd0e8f5131607611
Mona Mayrhofer <mona.mayrhofer@proton.me>
2026-03-05 12:13:53 +0100

    single attribute formatting

--- crates/wgsl_formatter/src/format/gen_attributes.rs
+++ crates/wgsl_formatter/src/format/gen_attributes.rs
@@ -1,12 +1,18 @@
-use parser::SyntaxToken;
+use dprint_core_macros::sc;
+use itertools::put_back;
+use parser::{SyntaxKind, SyntaxToken};
 use syntax::{
     AstNode as _,
-    ast::{self, Attribute},
+    ast::{self, Arguments, Attribute, IdentExpression},
 };
 
 use crate::format::{
-    ast_parse::{SyntaxIter, parse_many_comments_and_blankspace, parse_node_optional},
+    ast_parse::{
+        SyntaxIter, parse_end, parse_many_comments_and_blankspace, parse_node, parse_node_optional,
+        parse_token,
+    },
     gen_comments::gen_comments,
+    gen_function_call::gen_function_call_arguments,
     print_item_buffer::PrintItemBuffer,
     reporting::FormatDocumentResult,
 };
@@ -17,7 +23,7 @@ pub struct ParsedAttributes {
 
 pub fn parse_many_attributes(syntax: &mut SyntaxIter) -> FormatDocumentResult<ParsedAttributes> {
     // TODO(MonaMayrhofer) Think about whether this is the correct way to abstract this.
-    // Maybe there should even a "many with commments" combinator, to also deduplicate code from fn parameters/struct members
+    // Maybe there should even a "many with comments" combinator, to also deduplicate code from fn parameters/struct members
     // Also this is very similar to parse_many_comments_and_blankspace
     let mut attributes = Vec::new();
     loop {
@@ -46,6 +52,22 @@ pub fn gen_attributes(attributes: &ParsedAttributes) -> FormatDocumentResult<Pri
 }
 
 pub fn gen_attribute(attribute: &ast::Attribute) -> FormatDocumentResult<PrintItemBuffer> {
-    //formatted.push_sc(sc!("@"));
-    super::helpers::todo_verbatim(attribute.syntax())
+    let mut syntax = put_back(attribute.syntax().children_with_tokens());
+
+    parse_token(&mut syntax, SyntaxKind::AttributeOperator)?;
+    let item_comments_after_operator = parse_many_comments_and_blankspace(&mut syntax)?;
+    let item_identifier = parse_token(&mut syntax, parser::SyntaxKind::Identifier)?;
+    let item_comments_after_identifier = parse_many_comments_and_blankspace(&mut syntax)?;
+    let item_arguments = parse_node_optional::<Arguments>(&mut syntax);
+    parse_end(&mut syntax)?;
+
+    let mut formatted = PrintItemBuffer::new();
+    formatted.push_sc(sc!("@"));
+    formatted.extend(gen_comments(&item_comments_after_operator));
+    formatted.push_string(item_identifier.to_string());
+    formatted.extend(gen_comments(&item_comments_after_identifier));
+    if let Some(item_arguments) = item_arguments {
+        formatted.extend(gen_function_call_arguments(&item_arguments)?);
+    }
+    Ok(formatted)
 }
