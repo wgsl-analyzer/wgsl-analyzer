2764d5b45c8b9692790f2de144ebcc69b2dd7e2c
Mona Mayrhofer <mona.mayrhofer@proton.me>
2026-03-08 16:27:48 +0100

    Grouped no-space-before-semicolon tests

---
+++ crates/wgsl_formatter/src/tests/normative/no_space_before_semicolon.rs
@@ -0,0 +1,85 @@
+use expect_test::expect;
+
+use crate::test_util::check;
+
+/// Various tests that all make sure that there is no space before semicolons
+#[test]
+pub fn format_override_declaration_without_assignment_removes_space_before_semicolon() {
+    check(
+        "
+
+        override a /* a */ ;
+
+
+        ",
+        expect![[r#"
+            override a /* a */;
+        "#]],
+    );
+}
+
+#[test]
+pub fn format_const_declaration_has_no_space_before_semicolon() {
+    check(
+        "fn main() {
+
+        const a = 1 /* A */ ;
+
+
+        }",
+        expect![["
+            fn main() {
+                const a = 1 /* A */;
+            }
+        "]],
+    );
+}
+
+#[test]
+pub fn format_let_decl_has_no_space_before_semicolon() {
+    check(
+        "fn main() {
+
+        let a = 1 /* A */ ;
+
+
+        }",
+        expect![["
+            fn main() {
+                let a = 1 /* A */;
+            }
+        "]],
+    );
+}
+
+#[test]
+pub fn format_override_declaration_has_no_space_before_semicolon() {
+    check(
+        "
+
+        override a = 1 /* A */ ;
+
+
+        ",
+        expect![[r#"
+            override a = 1 /* A */;
+        "#]],
+    );
+}
+
+#[test]
+pub fn format_var_declaration_has_no_space_before_semicolon() {
+    check(
+        "fn main() {
+
+        var a = 1 /* A */ ;
+
+
+        }",
+        expect![["
+            fn main() {
+                var a = 1 /* A */;
+            }
+        "]],
+    );
+}
