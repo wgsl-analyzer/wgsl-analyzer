756a51b1382484795d4ef166fe6ba0cb272cdd63
Mona Mayrhofer <mona.mayrhofer@proton.me>
2026-03-17 16:33:08 +0100

    cleanup signals

--- crates/wgsl_formatter/src/format/multiline_group.rs
+++ crates/wgsl_formatter/src/format/multiline_group.rs
@@ -61,7 +61,7 @@ impl<'buffer> MultilineGroup<'buffer> {
         );
         self.start_reeval = Some(start_nl_condition.create_reevaluation());
         self.buffer.push_condition(start_nl_condition);
-        self.buffer.push_signal(Signal::StartNewLineGroup);
+        self.buffer.start_new_line_group();
 
         // TODO This is a bit of a shortcoming of the PBI api, we would want to write this after the "(", but can't because of the conditions between
         self.buffer.request(Request::Unconditional {
@@ -118,7 +118,7 @@ impl<'buffer> MultilineGroup<'buffer> {
     }
 
     pub fn end(&mut self) {
-        self.buffer.push_signal(Signal::FinishNewLineGroup);
+        self.buffer.finish_new_line_group();
         self.buffer.push_info(self.end_ln);
         if let Some(start_reeval) = self.start_reeval {
             self.buffer.push_reevaluation(start_reeval);
@@ -139,87 +139,3 @@ impl DerefMut for MultilineGroup<'_> {
         self.buffer
     }
 }
-
-// pub fn gen_multiline_group<I: IntoIterator<Item = PrintItemBuffer>>(lines: I) -> PrintItemBuffer {
-//     gen_surrounded_group(None, lines, None)
-// }
-
-// pub fn gen_surrounded_group<I: IntoIterator<Item = PrintItemBuffer>>(
-//     opener: Option<PrintItemBuffer>,
-//     lines: I,
-//     closer: Option<PrintItemBuffer>,
-// ) -> PrintItemBuffer {
-//     let mut formatted = PrintItemBuffer::new();
-
-//     let start_ln = LineNumber::new("start");
-//     let end_ln = LineNumber::new("end");
-//     let is_multiple_lines = create_is_multiple_lines_resolver(start_ln, end_ln);
-
-//     formatted.push_info(start_ln);
-//     formatted.push_anchor(LineNumberAnchor::new(end_ln));
-//     if let Some(opener) = opener {
-//         formatted.extend(opener);
-//     }
-
-//     let mut start_nl_condition = conditions::if_true_or(
-//         "paramMultilineStartIndent",
-//         Rc::clone(&is_multiple_lines),
-//         {
-//             let mut pi = PrintItems::default();
-//             pi.push_signal(Signal::NewLine);
-//             pi.push_signal(Signal::StartIndent);
-//             pi
-//         },
-//         {
-//             let mut pi = PrintItems::default();
-//             pi.push_signal(Signal::PossibleNewLine);
-//             pi
-//         },
-//     );
-//     let start_reeval = start_nl_condition.create_reevaluation();
-//     formatted.push_condition(start_nl_condition);
-//     formatted.push_signal(Signal::StartNewLineGroup);
-
-//     // TODO This is a bit of a shortcoming of the PBI api, we would want to write this after the "(", but can't because of the conditions between
-//     formatted.request(SeparationRequest::discouraged());
-
-//     for line in lines {
-//         formatted.extend(line);
-//         formatted.request_request(Request::Conditional {
-//             condition: Rc::clone(&is_multiple_lines),
-//             on_true: Box::new(RequestFolder::from(Request::Unconditional {
-//                 expected: BTreeSet::from([RequestItem::LineBreak]),
-//                 discouraged: BTreeSet::new(),
-//                 forced: BTreeSet::new(),
-//             })),
-//             on_false: Box::new(RequestFolder::from(Request::Unconditional {
-//                 expected: BTreeSet::from([RequestItem::Space]),
-//                 discouraged: BTreeSet::new(),
-//                 forced: BTreeSet::new(),
-//             })),
-//         });
-//     }
-
-//     // No trailing spaces
-//     formatted.discourage(RequestItem::Space);
-
-//     formatted.push_condition(conditions::if_true(
-//         "paramMultilineEndIndent",
-//         is_multiple_lines,
-//         {
-//             let mut pi = PrintItems::default();
-//             pi.push_signal(Signal::FinishIndent);
-//             pi
-//         },
-//     ));
-
-//     if let Some(closer) = closer {
-//         formatted.extend(closer);
-//     }
-
-//     formatted.push_signal(Signal::FinishNewLineGroup);
-//     formatted.push_info(end_ln);
-//     formatted.push_reevaluation(start_reeval);
-
-//     formatted
-// }
