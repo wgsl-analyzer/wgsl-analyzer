99b97ca0a227365d2ba633b10a65a9ba862ad29d
Benjamin Brienen <benjamin.brienen@outlook.com>
2026-03-26 21:13:44 +0100

    Update crates/hir_ty/src/infer.rs
    Co-authored-by: Alex Parkinson <okalex@gmail.com>

--- crates/hir_ty/src/infer.rs
+++ crates/hir_ty/src/infer.rs
@@ -1910,7 +1910,7 @@ impl<'database> InferenceContext<'database> {
                             r#type,
                             parameters: arguments
                                 .into_iter()
-                                .map(|(expression, r#type)| r#type)
+                                .map(|(_, r#type)| r#type)
                                 .collect(),
                         },
                     );
