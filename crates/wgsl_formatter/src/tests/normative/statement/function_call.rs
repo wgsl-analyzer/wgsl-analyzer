01a89b54820ec162fbaa19227652d701336591fd
Mona Mayrhofer <mona.mayrhofer@proton.me>
2026-03-27 16:08:02 +0100

    More test

--- crates/wgsl_formatter/src/tests/normative/statement/function_call.rs
+++ crates/wgsl_formatter/src/tests/normative/statement/function_call.rs
@@ -1,6 +1,9 @@
 use expect_test::expect;
 
-use crate::test_util::check;
+use crate::{
+    FormattingOptions,
+    test_util::{check, check_with_options},
+};
 
 #[test]
 pub fn format_function_call_statement_trailing_comma_with_multiline_arguments() {
@@ -89,33 +92,60 @@ pub fn format_function_call_statement_with_comment_has_no_trailing_whitespace() 
 }
 
 #[test]
-fn format_function_call_break_outer_arguments_leave_inner_alone() {
-    check(
+pub fn format_long_function_call_linewidth_within_inner_break_outer_arguments_leave_inner_alone() {
+    // Please note that the amount of "aaaa" in this test is carefully chosen to play with the line lengths.
+    // This the amount of aaa is such that, breaking the inner argument would satisfy the line width requirement.
+    // The formatter should prefer breaking the outer argument list, even tho breaking the inner one
+    // would be enough
+    check_with_options(
         "
         fn main() {
-            thingy(
-                aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa,
-                thing(1,2),
-            );
-
-            thingy(
-            thing(1,2),
-                aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa,
-            );
+            //Ruler:_|10_____20|_______30|_______40|_______50|_______60|_______70|_______80|
+            let a = thing(aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa,bla(2,aaaaaaaaa));
         }
-",
-        expect![[r#"
+        ",
+        &expect![[r#"
             fn main() {
-                thingy(
-                    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa,
-                    thing(1,2),
-                );
-
-                thingy(
-                    thing(1, 2),
-                    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa,
-                );
+                //Ruler:_|10_____20|_______30|_______40|_______50|_______60|_______70|_______80|
+                let a = thing(
+                        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa,
+                        bla(2, aaaaaaaaa),
+                    );
             }
         "#]],
+        &FormattingOptions {
+            width: 80,
+            ..Default::default()
+        },
+        parser::Edition::LATEST,
+    );
+}
+
+#[test]
+pub fn format_long_type_alias_linewidth_outside_inner_break_outer_arguments_leave_inner_alone() {
+    // Please note that the amount of "aaaa" in this test is carefully chosen to play with the line lengths.
+    // This the amount of aaa is such that, breaking the inner argument would still not satisfy the line width
+    // requirement.
+    check_with_options(
+        "
+        fn main() {
+            //Ruler:_|10_____20|_______30|_______40|_______50|_______60|_______70|_______80|
+            let a = thing(aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa,bla(2,aaaaaaaaa));
+        }
+        ",
+        &expect![[r#"
+            fn main() {
+                //Ruler:_|10_____20|_______30|_______40|_______50|_______60|_______70|_______80|
+                let a = thing(
+                        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa,
+                        bla(2, aaaaaaaaa),
+                    );
+            }
+        "#]],
+        &FormattingOptions {
+            width: 80,
+            ..Default::default()
+        },
+        parser::Edition::LATEST
     );
 }
--- crates/wgsl_formatter/src/tests/normative/types.rs
+++ crates/wgsl_formatter/src/tests/normative/types.rs
@@ -1,6 +1,9 @@
 use expect_test::expect;
 
-use crate::test_util::check;
+use crate::{
+    FormattingOptions,
+    test_util::{check, check_with_options},
+};
 
 #[test]
 pub fn format_type_multiline_template_gets_broken_into_multiple_lines() {
@@ -39,5 +42,56 @@ pub fn format_type_nested_multiline_template_gets_broken_into_multiple_lines() {
                 18,
             >;
         "#]],
+    );
+}
+
+#[test]
+pub fn format_long_type_alias_linewidth_within_inner_break_outer_arguments_leave_inner_alone() {
+    // Please note that the amount of "aaaa" in this test is carefully chosen to play with the line lengths.
+    // This the amount of aaa is such that, breaking the inner argument would satisfy the line width requirement.
+    // The formatter should prefer breaking the outer argument list, even tho breaking the inner one
+    // would be enough
+    check_with_options(
+        "
+        //Ruler:_|10_____20|_______30|_______40|_______50|_______60|_______70|_______80|
+        alias Test = array<aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa,array<2,aaaaaaaaa>>;
+        ",
+        &expect![[r#"
+            //Ruler:_|10_____20|_______30|_______40|_______50|_______60|_______70|_______80|
+            alias Test = array<
+                aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa,
+                array<2, aaaaaaaaa>,
+            >;
+        "#]],
+        &FormattingOptions {
+            width: 80,
+            ..Default::default()
+        },
+        parser::Edition::LATEST,
+    );
+}
+
+#[test]
+pub fn format_long_type_alias_linewidth_outside_inner_break_outer_arguments_leave_inner_alone() {
+    // Please note that the amount of "aaaa" in this test is carefully chosen to play with the line lengths.
+    // This the amount of aaa is such that, breaking the inner argument would still not satisfy the line width
+    // requirement.
+    check_with_options(
+        "
+        //Ruler:_|10_____20|_______30|_______40|_______50|_______60|_______70|_______80|
+        alias Test = array<aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa,array<2,aaaaaaaaa>>;
+        ",
+        &expect![[r#"
+            //Ruler:_|10_____20|_______30|_______40|_______50|_______60|_______70|_______80|
+            alias Test = array<
+                aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa,
+                array<2, aaaaaaaaa>,
+            >;
+        "#]],
+        &FormattingOptions {
+            width: 80,
+            ..Default::default()
+        },
+        parser::Edition::LATEST
     );
 }
