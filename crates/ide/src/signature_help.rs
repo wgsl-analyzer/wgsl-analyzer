23c56bd08c2b3e303896fb17a08a82e94846b1f8
Benjamin Brienen <benjamin.brienen@outlook.com>
2026-03-31 19:04:21 +0200

    Support multiple signature helps, active signature, active parameter (#973)
    # Objective
    
    Signature help should show all overloads.
    
    ## Solution
    
    Look up functions using `process_all_names` instead of literally
    performing inferance. A possible improvement would be to do advanced
    type checking logic to further reduce the number of overloads shown.
    This also does not show builtins yet.
    
    ## Testing
    
    Added unit tests
    
    ## Showcase
    
    <img width="415" height="176" alt="image"
    src="https://github.com/user-attachments/assets/dc74d040-759b-49a1-a576-dd30dd52646e"
    />
    
    <br>
    <img width="415" height="176" alt="image"
    src="https://github.com/user-attachments/assets/371b7c92-7bf2-4539-83e4-8181f089c482"
    />
    
    <br>
    <img width="415" height="176" alt="image"
    src="https://github.com/user-attachments/assets/19e307d8-3add-4c00-82ab-9a6054b2c467"
    />

--- crates/ide/src/signature_help.rs
+++ crates/ide/src/signature_help.rs
@@ -1,16 +1,22 @@
 use base_db::{EditionedFileId, FilePosition, TextSize};
-use hir::Semantics;
+use hir::{HirDatabase as _, Semantics, database::DefDatabase};
+use hir_def::{
+    database::{InternDatabase as _, Location},
+    item_tree::ModuleItem,
+    resolver::ScopeDef,
+};
 use hir_ty::{
     function::FunctionDetails,
     infer::ResolvedCall,
-    ty::pretty::{TypeVerbosity, pretty_fn_inner_with_offsets},
+    ty::pretty::{TypeVerbosity, pretty_fn_inner_with_offsets, pretty_fn_with_verbosity},
 };
 use ide_db::RootDatabase;
-use rowan::TextRange;
+use rowan::{TextLen, TextRange};
 use syntax::{AstNode as _, SyntaxKind, SyntaxToken, ast};
 
 /// Signature help information for a function call.
 /// This must store a vec because WGSL has function overloading.
+/// Same as `lsp_types::SignatureHelp`.
 #[derive(Debug)]
 pub struct SignatureHelp {
     pub signatures: Vec<OverloadSignatureHelp>,
@@ -98,32 +104,31 @@ pub(crate) fn signature_help(
     let (function_call, _, active_parameter) = find_enclosing_call(&token, position.offset)?;
 
     // Try to resolve the function call via type inference
-    let container = semantics.find_container(file_id, function_call.syntax())?;
-    let def_with_body = container.as_def_with_body_id()?;
-    let analyzed = semantics.analyze(def_with_body);
+    let text = function_call.ident_expression()?.syntax().text();
+    let mut overloads = Vec::new();
+    semantics
+        .resolver(file_id, syntax)
+        .process_all_names(|name, scope_def| {
+            if name.as_str().to_owned().contains(&text.to_string())
+                && let ScopeDef::ModuleItem(file_id, ModuleItem::Function(module_item_id)) =
+                    scope_def
+            {
+                overloads.push(module_item_id);
+            }
+        });
 
-    let call_expr = ast::Expression::FunctionCall(function_call);
-    let expression_id = analyzed.expression_id(&call_expr);
-
-    let mut signatures = Vec::new();
+    let mut signatures: Vec<_> = overloads
+        .into_iter()
+        .map(|id| database.intern_function(Location::new(file_id, id)))
+        .map(|function_id| database.function_type(function_id))
+        .map(|function_type| function_type.lookup(database))
+        .filter_map(|function| {
+            let length: u32 = function.parameters.len().try_into().unwrap();
+            (active_parameter.is_none() || active_parameter.is_some_and(|index| index < length))
+                .then(|| build_signature(database, &function, None))
+        })
+        .collect();
     let mut active_signature = None;
-
-    if let Some(expr_id) = expression_id
-        && let Some(resolved) = analyzed.infer.call_resolution(expr_id)
-    {
-        match resolved {
-            ResolvedCall::Function(func_id) => {
-                let function = func_id.lookup(database);
-                // TODO: add documentation
-                // https://github.com/wgsl-analyzer/wgsl-analyzer/issues/971
-                signatures.push(build_signature(database, &function, None));
-                active_signature = Some(0);
-            },
-            // TODO: implement signature help for other kinds of calls
-            // https://github.com/wgsl-analyzer/wgsl-analyzer/issues/970
-            ResolvedCall::OtherTypeInitializer(_) => return None,
-        }
-    }
 
     if signatures.is_empty() {
         return None;
@@ -132,32 +137,36 @@ pub(crate) fn signature_help(
     Some(SignatureHelp {
         signatures,
         active_signature,
-        active_parameter: Some(active_parameter),
+        active_parameter,
     })
 }
 
 fn find_enclosing_call(
     token: &SyntaxToken,
     offset: TextSize,
-) -> Option<(ast::FunctionCall, ast::Arguments, u32)> {
+) -> Option<(ast::FunctionCall, ast::Arguments, Option<u32>)> {
     // Walk up ancestors to find an Arguments node
     for ancestor in token.parent_ancestors() {
         if let Some(arguments) = ast::Arguments::cast(ancestor.clone()) {
             // The parent of Arguments should be a FunctionCall
+            // add support for builtins
             let function_call = ast::FunctionCall::cast(ancestor.parent()?)?;
+            if arguments.arguments().next().is_none() {
+                return Some((function_call, arguments, None));
+            }
 
             // Count commas before the cursor to determine active parameter
-            let mut param_index: u32 = 0;
+            let mut parameter_index: u32 = 0;
             for child in arguments.syntax().children_with_tokens() {
                 if child.text_range().start() >= offset {
                     break;
                 }
                 if child.kind() == SyntaxKind::Comma {
-                    param_index += 1;
+                    parameter_index += 1;
                 }
             }
 
-            return Some((function_call, arguments, param_index));
+            return Some((function_call, arguments, Some(parameter_index)));
         }
     }
     None
@@ -184,5 +193,74 @@ fn build_signature(
         signature,
         active_parameter: None,
         parameters,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use test_utils::extract_offset;
+
+    use crate::Analysis;
+
+    use super::*;
+
+    #[test]
+    fn find_enclosing_call_works_0() {
+        let text = r#"
+fn foo() {
+    bar($0);
+}
+fn bar(x: u32, y: bool) -> f32 { 0.0f }
+"#;
+
+        let (offset, text) = extract_offset(text);
+        let (analysis, file_id) = Analysis::from_single_file(text);
+        let semantics = Semantics::new(&analysis.database);
+        let file_id = EditionedFileId::from_file(&analysis.database, file_id);
+        let source_file = semantics.parse(file_id);
+        let syntax = source_file.syntax();
+        let token = syntax.token_at_offset(offset).left_biased().unwrap();
+        let result = find_enclosing_call(&token, offset).unwrap();
+        assert_eq!(result.2, None);
+    }
+
+    #[test]
+    fn find_enclosing_call_works_1() {
+        let text = r#"
+fn foo() {
+    bar(1$0);
+}
+fn bar(x: u32, y: bool) -> f32 { 0.0f }
+"#;
+
+        let (offset, text) = extract_offset(text);
+        let (analysis, file_id) = Analysis::from_single_file(text);
+        let semantics = Semantics::new(&analysis.database);
+        let file_id = EditionedFileId::from_file(&analysis.database, file_id);
+        let source_file = semantics.parse(file_id);
+        let syntax = source_file.syntax();
+        let token = syntax.token_at_offset(offset).left_biased().unwrap();
+        let result = find_enclosing_call(&token, offset).unwrap();
+        assert_eq!(result.2, Some(0));
+    }
+
+    #[test]
+    fn find_enclosing_call_works2() {
+        let text = r#"
+fn foo() {
+    bar(1, $0);
+}
+fn bar(x: u32, y: bool) -> f32 { 0.0f }
+"#;
+
+        let (offset, text) = extract_offset(text);
+        let (analysis, file_id) = Analysis::from_single_file(text);
+        let semantics = Semantics::new(&analysis.database);
+        let file_id = EditionedFileId::from_file(&analysis.database, file_id);
+        let source_file = semantics.parse(file_id);
+        let syntax = source_file.syntax();
+        let token = syntax.token_at_offset(offset).left_biased().unwrap();
+        let result = find_enclosing_call(&token, offset).unwrap();
+        assert_eq!(result.2, Some(1));
     }
 }
