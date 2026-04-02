a1e4fe029e069e5059965cbddf90e87322066b9b
Mona Mayrhofer <mona.mayrhofer@proton.me>
2026-04-02 14:42:05 +0200

    tests

--- crates/wgsl_formatter/src/tests/normative/statement/switch.rs
+++ crates/wgsl_formatter/src/tests/normative/statement/switch.rs
@@ -116,3 +116,34 @@ pub fn format_switch_statement_case_trailing_comma_gets_removed() {
         "#]],
     );
 }
+
+#[test]
+pub fn format_switch_statement_collapse_to_one_line() {
+    // https://discord.com/channels/1289346613185351722/1341941812675481680/1488979360772784239
+    // We collapse cases to one line if they fit on one line. Not all cases must have the same
+    // "collapsedness"
+    check(
+        "fn main() {
+            switch(a) {
+                case 1 {
+                    let a = 1;
+                }
+                case 2 {
+                    let a = 1;
+                    let a = 1;
+                }
+            }
+        }",
+        expect![[r#"
+            fn main() {
+                switch a {
+                    case 1 { let a = 1; }
+                    case 2 {
+                        let a = 1;
+                        let a = 1;
+                    }
+                }
+            }
+        "#]],
+    );
+}
