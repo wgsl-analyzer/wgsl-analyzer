2469aae2416669a26d60a2d3b97b5358e6523fcc
Mona Mayrhofer <mona.mayrhofer@proton.me>
2026-04-03 18:23:13 +0200

    fix const attribute

--- crates/wgsl_formatter/src/format/gen_attributes.rs
+++ crates/wgsl_formatter/src/format/gen_attributes.rs
@@ -5,7 +5,7 @@ use itertools::put_back;
 use parser::SyntaxKind;
 use syntax::{
     AstNode as _,
-    ast::{self, Arguments, DiagnosticControl},
+    ast::{self, Arguments, Attribute, DiagnosticControl},
 };
 
 use crate::format::{
@@ -23,7 +23,7 @@ use crate::format::{
 use super::print_item_buffer::request_folder::RequestItem;
 
 pub struct ParsedAttribute {
-    attribute: ast::Attribute,
+    attribute: Attribute,
     comments_after_attribute: Vec<Comment>,
 }
 pub struct ParsedAttributes {
@@ -36,7 +36,7 @@ pub fn parse_many_attributes(syntax: &mut SyntaxIter) -> FormatDocumentResult<Pa
     // Also this is very similar to parse_many_comments_and_blankspace
     let mut attributes = Vec::new();
     loop {
-        let Some(item_attribute) = parse_node_optional::<ast::Attribute>(syntax) else {
+        let Some(item_attribute) = parse_node_optional::<Attribute>(syntax) else {
             break;
         };
         let item_comments_after_attribute = parse_many_comments_and_blankspace(syntax)?;
@@ -74,11 +74,13 @@ pub fn gen_attributes(
 
     for attribute in &attributes.attributes {
         match &attribute.attribute {
-            ast::Attribute::ConstantAttribute(_) => {},
-            ast::Attribute::DiagnosticAttribute(_) => {
+            Attribute::ConstantAttribute(_) => {
+                attribute_group_pre_fn_inlined.push((0, attribute));
+            },
+            Attribute::DiagnosticAttribute(_) => {
                 attribute_group_diagnostics.push((0, attribute));
             },
-            ast::Attribute::OtherAttribute(attrib) => {
+            Attribute::OtherAttribute(attrib) => {
                 let name = attrib.name().map(|identifier| identifier.text().to_owned());
                 let name = name.as_deref();
                 match name {
@@ -86,7 +88,6 @@ pub fn gen_attributes(
                     Some("align") => attribute_group_offset_align_size.push((1, attribute)),
                     Some("size") => attribute_group_offset_align_size.push((2, attribute)),
 
-                    Some("const") => attribute_group_pre_fn_inlined.push((0, attribute)),
                     Some("must_use") => attribute_group_pre_fn_inlined.push((1, attribute)),
 
                     Some("group") => attribute_group_binding_group.push((0, attribute)),
@@ -161,15 +162,17 @@ pub fn gen_attributes(
     Ok(formatted)
 }
 
-pub fn gen_attribute(attribute: &ast::Attribute) -> FormatDocumentResult<PrintItemBuffer> {
+pub fn gen_attribute(attribute: &Attribute) -> FormatDocumentResult<PrintItemBuffer> {
     match attribute {
-        ast::Attribute::ConstantAttribute(constant_attribute) => todo!(),
-        ast::Attribute::DiagnosticAttribute(diagnostic_attribute) => {
+        Attribute::ConstantAttribute(constant_attribute) => gen_const_attribute(constant_attribute),
+        Attribute::DiagnosticAttribute(diagnostic_attribute) => {
             gen_diagnostic_attribute(diagnostic_attribute)
         },
-        ast::Attribute::OtherAttribute(other_attribute) => gen_other_attribute(other_attribute),
+        Attribute::OtherAttribute(other_attribute) => gen_other_attribute(other_attribute),
     }
 }
+
+//TODO(MonaMayrhofer) Collapse this attribute gen logic into something more generic
 pub fn gen_diagnostic_attribute(
     attribute: &ast::DiagnosticAttribute
 ) -> FormatDocumentResult<PrintItemBuffer> {
@@ -207,6 +210,29 @@ pub fn gen_other_attribute(
     formatted.push_sc(sc!("@"));
     formatted.extend(gen_comments(&item_comments_after_operator));
     formatted.push_string(item_identifier.to_string());
+    formatted.extend(gen_comments(&item_comments_after_identifier));
+    if let Some(item_arguments) = item_arguments {
+        formatted.extend(gen_function_call_arguments(&item_arguments)?);
+    }
+    Ok(formatted)
+}
+
+pub fn gen_const_attribute(
+    attribute: &ast::ConstantAttribute
+) -> FormatDocumentResult<PrintItemBuffer> {
+    let mut syntax = put_back(attribute.syntax().children_with_tokens());
+
+    parse_token(&mut syntax, SyntaxKind::AttributeOperator)?;
+    let item_comments_after_operator = parse_many_comments_and_blankspace(&mut syntax)?;
+    parse_token(&mut syntax, parser::SyntaxKind::Const)?;
+    let item_comments_after_identifier = parse_many_comments_and_blankspace(&mut syntax)?;
+    let item_arguments = parse_node_optional::<Arguments>(&mut syntax);
+    parse_end(&mut syntax)?;
+
+    let mut formatted = PrintItemBuffer::new();
+    formatted.push_sc(sc!("@"));
+    formatted.extend(gen_comments(&item_comments_after_operator));
+    formatted.push_sc(sc!("const"));
     formatted.extend(gen_comments(&item_comments_after_identifier));
     if let Some(item_arguments) = item_arguments {
         formatted.extend(gen_function_call_arguments(&item_arguments)?);
