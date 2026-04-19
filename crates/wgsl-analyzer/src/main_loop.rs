81411d724147f92ede233b181c6538ae1ef5516e
Benjamin Brienen <benjamin.brienen@outlook.com>
2026-04-19 18:06:26 +0200

    remove unused/useless LSP code (#1043)
    # Objective
    
    This stuff can just be removed. Probably more, but this stood out.
    
    ## Solution
    
    Delete.
    
    ## Testing
    
    Nothing seems broken when I run it locally.

--- crates/wgsl-analyzer/src/main_loop.rs
+++ crates/wgsl-analyzer/src/main_loop.rs
@@ -956,7 +956,6 @@ impl GlobalState {
                 },
             )
             .on::<NO_RETRY, lsp::extensions::ViewSyntaxTree>(handlers::request::view_syntax_tree)
-            .on::<NO_RETRY, lsp::extensions::DebugCommand>(handlers::request::debug_command)
             .on::<NO_RETRY, lsp::extensions::FullSource>(handlers::request::full_source)
             .on::<RETRY, lsp::extensions::ViewPackageGraph>(
                 handlers::request::handle_view_package_graph,
