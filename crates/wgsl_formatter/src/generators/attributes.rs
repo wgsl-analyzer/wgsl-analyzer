7fb509ed5c70a37ccf19bcc58e2fada4c71c967c
Mona Mayrhofer <mona.mayrhofer@proton.me>
2026-07-10 14:47:38 +0200

    annotate and sqish TODOs

--- crates/wgsl_formatter/src/generators/attributes.rs
+++ crates/wgsl_formatter/src/generators/attributes.rs
@@ -43,7 +43,7 @@ pub struct ParsedAttributes {
 }
 
 pub fn parse_many_attributes(syntax: &mut SyntaxIter) -> FormatDocumentResult<ParsedAttributes> {
-    // TODO(MonaMayrhofer) Think about whether this is the correct way to abstract this.
+    // TODO(MonaMayrhofer,outdated=None of the alternative i have tried felt right. These things look similar, but are fundamentally different in behavior) Think about whether this is the correct way to abstract this.
     // Maybe there should even a "many with comments" combinator, to also deduplicate code from fn parameters/struct members
     // Also this is very similar to parse_many_comments_and_blankspace
     let mut attributes = Vec::new();
@@ -290,7 +290,7 @@ pub fn gen_interpolate_attribute(
     let interpolate_type_name = parse_node::<InterpolateTypeName>(&mut syntax)?;
     let item_comments_after_itn = parse_many_comments_and_blankspace(&mut syntax)?;
 
-    // TODO(MonaMayrhofer) i am not really proud of this code, there must be a cleaner way
+    // TODO(MonaMayrhofer,outdated=Its fine, not worth the effort) i am not really proud of this code, there must be a cleaner way
     let sampling = if parse_token_optional(&mut syntax, SyntaxKind::Comma).is_some() {
         let item_comments_after_comma = parse_many_comments_and_blankspace(&mut syntax)?;
         let interpolate_sampling_name = parse_node::<InterpolateSamplingName>(&mut syntax)?;
