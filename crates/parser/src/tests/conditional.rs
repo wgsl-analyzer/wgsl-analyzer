ca316839fd9d5c6162b9fe45d7df4e9f3a4a9b26
Benjamin Brienen <Benjamin.Brienen@outlook.com>
2026-07-22 01:02:46 +0200

    fix for switch cases with test

--- crates/parser/src/tests/conditional.rs
+++ crates/parser/src/tests/conditional.rs
@@ -218,3 +218,148 @@ fn foo(){}
             error at 27..28: invalid syntax, expected one of: 'alias', '@', 'const', 'const_assert', 'diagnostic', 'enable', 'fn', 'import', 'let', 'override', 'requires', 'struct', 'var'"#]],
     );
 }
+
+#[test]
+fn on_switch_cases() {
+    check_with_edition(
+        edition::Edition::Wesl2025Unstable,
+        "
+fn foo() {
+    switch true {
+        @if(true)
+        case true, false: { return; }
+        @elif(false)
+        case true, false: { return; }
+        @if(true)
+        default: { return; }
+        @else
+        default: { return; }
+    }
+}
+        ",
+        expect![[r#"
+            SourceFile@0..251
+              Blankspace@0..1 "\n"
+              FunctionDeclaration@1..242
+                Fn@1..3 "fn"
+                Blankspace@3..4 " "
+                Name@4..7
+                  Identifier@4..7 "foo"
+                FunctionParameters@7..9
+                  ParenthesisLeft@7..8 "("
+                  ParenthesisRight@8..9 ")"
+                Blankspace@9..10 " "
+                CompoundStatement@10..242
+                  BraceLeft@10..11 "{"
+                  Blankspace@11..16 "\n    "
+                  SwitchStatement@16..240
+                    Switch@16..22 "switch"
+                    Blankspace@22..23 " "
+                    Literal@23..27
+                      True@23..27 "true"
+                    Blankspace@27..28 " "
+                    SwitchBody@28..240
+                      BraceLeft@28..29 "{"
+                      Blankspace@29..38 "\n        "
+                      IfAttribute@38..47
+                        AttributeOperator@38..39 "@"
+                        If@39..41 "if"
+                        ParenthesisLeft@41..42 "("
+                        Literal@42..46
+                          True@42..46 "true"
+                        ParenthesisRight@46..47 ")"
+                      Blankspace@47..56 "\n        "
+                      SwitchBodyCase@56..85
+                        Case@56..60 "case"
+                        Blankspace@60..61 " "
+                        SwitchCaseSelectors@61..72
+                          Literal@61..65
+                            True@61..65 "true"
+                          Comma@65..66 ","
+                          Blankspace@66..67 " "
+                          Literal@67..72
+                            False@67..72 "false"
+                        Colon@72..73 ":"
+                        Blankspace@73..74 " "
+                        CompoundStatement@74..85
+                          BraceLeft@74..75 "{"
+                          Blankspace@75..76 " "
+                          ReturnStatement@76..83
+                            Return@76..82 "return"
+                            Semicolon@82..83 ";"
+                          Blankspace@83..84 " "
+                          BraceRight@84..85 "}"
+                      Blankspace@85..94 "\n        "
+                      ElifAttribute@94..106
+                        AttributeOperator@94..95 "@"
+                        Elif@95..99 "elif"
+                        ParenthesisLeft@99..100 "("
+                        Literal@100..105
+                          False@100..105 "false"
+                        ParenthesisRight@105..106 ")"
+                      Blankspace@106..115 "\n        "
+                      SwitchBodyCase@115..144
+                        Case@115..119 "case"
+                        Blankspace@119..120 " "
+                        SwitchCaseSelectors@120..131
+                          Literal@120..124
+                            True@120..124 "true"
+                          Comma@124..125 ","
+                          Blankspace@125..126 " "
+                          Literal@126..131
+                            False@126..131 "false"
+                        Colon@131..132 ":"
+                        Blankspace@132..133 " "
+                        CompoundStatement@133..144
+                          BraceLeft@133..134 "{"
+                          Blankspace@134..135 " "
+                          ReturnStatement@135..142
+                            Return@135..141 "return"
+                            Semicolon@141..142 ";"
+                          Blankspace@142..143 " "
+                          BraceRight@143..144 "}"
+                      Blankspace@144..153 "\n        "
+                      IfAttribute@153..162
+                        AttributeOperator@153..154 "@"
+                        If@154..156 "if"
+                        ParenthesisLeft@156..157 "("
+                        Literal@157..161
+                          True@157..161 "true"
+                        ParenthesisRight@161..162 ")"
+                      Blankspace@162..171 "\n        "
+                      SwitchBodyCase@171..191
+                        Default@171..178 "default"
+                        Colon@178..179 ":"
+                        Blankspace@179..180 " "
+                        CompoundStatement@180..191
+                          BraceLeft@180..181 "{"
+                          Blankspace@181..182 " "
+                          ReturnStatement@182..189
+                            Return@182..188 "return"
+                            Semicolon@188..189 ";"
+                          Blankspace@189..190 " "
+                          BraceRight@190..191 "}"
+                      Blankspace@191..200 "\n        "
+                      ElseAttribute@200..205
+                        AttributeOperator@200..201 "@"
+                        Else@201..205 "else"
+                      Blankspace@205..214 "\n        "
+                      SwitchBodyCase@214..234
+                        Default@214..221 "default"
+                        Colon@221..222 ":"
+                        Blankspace@222..223 " "
+                        CompoundStatement@223..234
+                          BraceLeft@223..224 "{"
+                          Blankspace@224..225 " "
+                          ReturnStatement@225..232
+                            Return@225..231 "return"
+                            Semicolon@231..232 ";"
+                          Blankspace@232..233 " "
+                          BraceRight@233..234 "}"
+                      Blankspace@234..239 "\n    "
+                      BraceRight@239..240 "}"
+                  Blankspace@240..241 "\n"
+                  BraceRight@241..242 "}"
+              Blankspace@242..251 "\n        ""#]],
+    );
+}
