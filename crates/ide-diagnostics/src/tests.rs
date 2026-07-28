5e2b7e1a234f783a3aee8aac187fc6b322062ed9
Benjamin Brienen <Benjamin.Brienen@outlook.com>
2026-07-28 15:57:43 +0200

    infer field on builtin struct no such field coverage

--- crates/ide-diagnostics/src/tests.rs
+++ crates/ide-diagnostics/src/tests.rs
@@ -76,11 +76,21 @@ fn infer_assert_expect() {
 }
 
 #[test]
-fn infer_field_scalar() {
+fn infer_field_scalar_no_such_field() {
     check_diagnostics(
         "fn foo() { let x = 1; let y = x.nonsense; }",
         expect![[r#"
             30..40 wgsl-analyzer Error 3: no field `nonsense` on type i32
+        "#]],
+    );
+}
+
+#[test]
+fn infer_field_builtin_struct_no_such_field() {
+    check_diagnostics(
+        "fn foo() { let x = modf(1.0); let y = x.nonsense; }",
+        expect![[r#"
+            38..48 wgsl-analyzer Error 3: no field `nonsense` on type __modf_result_abstract
         "#]],
     );
 }
