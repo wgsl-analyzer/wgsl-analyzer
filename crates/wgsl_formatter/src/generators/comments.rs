7b3f8b4e8d85b7c473feb3fde1f211a2c6bc5946
Mona Mayrhofer <mona.mayrhofer@proton.me>
2026-08-04 18:37:34 +0200

    fix attributes in loop and continuing

--- crates/wgsl_formatter/src/generators/comments.rs
+++ crates/wgsl_formatter/src/generators/comments.rs
@@ -44,6 +44,7 @@ pub fn parse_comment_optional(syntax: &mut SyntaxIter) -> Option<Comment> {
     clippy::unnecessary_wraps,
     reason = "Keep the API homogeneous with all gen_* functions"
 )]
+#[deprecated]
 pub fn parse_many_comments_and_blankspace(
     syntax: &mut SyntaxIter
 ) -> FormatDocumentResult<Vec<Comment>> {
@@ -54,6 +55,7 @@ pub fn parse_many_comments_and_blankspace(
     clippy::redundant_pattern_matching,
     reason = "Make it more obvious that the syntax token is consumed"
 )]
+#[deprecated]
 pub fn infallible_parse_many_comments_and_blankspace(syntax: &mut SyntaxIter) -> Vec<Comment> {
     let mut comments = Vec::new();
     loop {
