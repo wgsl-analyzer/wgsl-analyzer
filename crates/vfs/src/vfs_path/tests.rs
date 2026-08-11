bc851c05c644a940ce57083fef3ed50d99ffba0e
Stefnotch <brandmairstefan@gmail.com>
2026-08-11 15:02:02 +0200

    Expose virtual paths

--- crates/vfs/src/vfs_path/tests.rs
+++ crates/vfs/src/vfs_path/tests.rs
@@ -43,3 +43,12 @@ fn root_virtual_path() {
         ))))
     );
 }
+
+#[test]
+fn virtual_path_components() {
+    let path = VirtualPath::new("/foo/bar/cat.wesl".to_owned());
+    let mut components = path.components();
+    assert_eq!(components.next(), Some("foo"));
+    assert_eq!(components.next(), Some("bar"));
+    assert_eq!(components.next(), Some("cat.wesl"));
+}
