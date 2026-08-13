6d712c6803a7da383534acc920141e007267de92
Mona Mayrhofer <mona.mayrhofer@proton.me>
2026-08-13 17:52:34 +0200

    deal with empty statements

--- crates/wgsl_formatter/src/generators/node.rs
+++ crates/wgsl_formatter/src/generators/node.rs
@@ -240,8 +240,10 @@ pub fn gen_node(
         SyntaxKind::ForCondition => with_node!(gen_for_statement_condition, node),
         SyntaxKind::ForContinuingPart => with_node!(gen_for_statement_continuing_part, node),
 
-//TODO(MonaMayrhofer,now)
-        SyntaxKind::EmptyStatement => Ok(PrintItemBuffer::default()),
+        SyntaxKind::EmptyStatement => {
+            // We remove lonely semicolons
+            Ok(PrintItemBuffer::default())
+        },
 
         SyntaxKind::Alias => with_sc!("alias"),
         SyntaxKind::Break => with_sc!("break"),
