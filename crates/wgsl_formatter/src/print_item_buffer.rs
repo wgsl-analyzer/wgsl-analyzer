3921cf0dec13b02564c9d23e145e9f72df7388c3
Mona Mayrhofer <mona.mayrhofer@proton.me>
2026-08-15 19:20:05 +0200

    add docs to printitembuffer

--- crates/wgsl_formatter/src/print_item_buffer.rs
+++ crates/wgsl_formatter/src/print_item_buffer.rs
@@ -17,6 +17,10 @@ use crate::print_item_buffer::spacing_request::Request;
 //   * Cons: Very verbose, imperative and brittle ("forget to update the last pushed item"), cannot deal with rule 4 properly.
 // * "Cleverly" structure code and where to put spaces, so that these cases are implicitly dealt with
 //   * Cons: "Clever" code that doesn't explicitly state intent, and thus is brittle, new requirements might require big restructurings
+// * Excessive use of dprint's conditionals to check "was the last item a space"
+//   * This would probably work, but dprint's conditionals are a lot less lightweight than a Request (which basically is 3 u8's that get and-ed)
+//   * dprint's conditionals have a shortcoming in that they can't represent the intent of "no matter what comes next, i don't want a
+//     space here". With conditionals we would have to check everywhere we push spaces if a space is allowed here.
 // * Re-parse the AST into a formatting-ast which tracks comments etc.
 //   * We already do that. The formatting-ast is built and immediately destroyed by the parse->format structure that
 //     the gen_*_ functions are built with. While it makes the code cleaner, it does not solve the problem.
@@ -52,6 +56,13 @@ pub struct PrintItemBuffer {
 }
 
 impl PrintItemBuffer {
+    /// Pushes a [`Request`] onto the buffer.
+    ///
+    /// Requests that are added consecutively are combined together.
+    ///
+    /// Generally you don't need to worry about when Requests are combined, as that
+    /// procedure is commutative - so conceptually Requests only get combined once
+    /// all the Buffers get combined together (with [`Self::extend`]).
     pub fn request(
         &mut self,
         incoming_request: Request,
@@ -79,12 +90,41 @@ impl PrintItemBuffer {
         pi
     }
 
+    /// Forces the trailing requests in this buffer to be applied immediately, preventing further Requests from merging with them.
+    ///
+    /// Normally consecutive Requests get combined (so 2 spaces would get collapsed into one space for example). However
+    /// if [`Self::apply_end_request`] is called in between two requests, they will not get combined.
+    ///
+    /// ```rust
+    /// # use crate::wgsl_formatter::print_item_buffer::spacing_request::{Request, RequestItem};
+    /// # use crate::wgsl_formatter::print_item_buffer::PrintItemBuffer;
+    /// let formatted = dprint_core::formatting::format(
+    ///     || {
+    ///         let mut formatted = PrintItemBuffer::default();
+    ///         formatted.push_sc(dprint_core_macros::sc!("|"));
+    ///         formatted.request(Request::expect(RequestItem::Space));
+    ///         formatted.request(Request::expect(RequestItem::Space));
+    ///         formatted.push_sc(dprint_core_macros::sc!("|"));
+    ///         formatted.request(Request::expect(RequestItem::Space));
+    ///         formatted.apply_end_request();
+    ///         formatted.request(Request::expect(RequestItem::Space));
+    ///         formatted.push_sc(dprint_core_macros::sc!("|"));
+    ///         formatted.finish()
+    ///     },
+    ///     dprint_core::formatting::PrintOptions {
+    ///         max_width: 80,
+    ///         indent_width: 4,
+    ///         use_tabs: false,
+    ///         new_line_text: "\n",
+    ///     },
+    /// );
+    /// assert_eq!(formatted, "| |  |")
+    /// ```
     pub fn apply_end_request(&mut self) {
         std::mem::take(&mut self.end_request).resolve(&mut self.items);
     }
 
-    // ==== Helper Methods ====
-
+    /// Appends another [`PrintItemBuffer`] onto this one.
     pub fn extend(
         &mut self,
         other: Self,
@@ -102,29 +142,44 @@ impl PrintItemBuffer {
         self.request(other.end_request);
     }
 
+    /// Applies trailing requests and pushes a string to the buffer whose content is not yet known at compile time.
+    ///
+    /// The string may not contain newlines or tabs - those need to be pushed separately via [`Self::push_tab`] or [`Self::request`].
+    /// Prefer using [`Self::push_sc`] whenever possible.
+    ///
+    /// # Panics
+    /// If compiled with prefer-immediate-crash this function will panic if the string contains newlines or tabs.
     pub fn push_string(
         &mut self,
         string: String,
     ) {
         #[cfg(feature = "prefer-immediate-crash")]
         {
-            if string.contains("\n") {
-                panic!("Cannot push string with newlines to PrintItemBuffer {string:?}");
-            }
-            if string.contains("\t") {
-                panic!("Cannot push string with tabs to PrintItemBuffer {string:?}");
-            }
+            assert!(
+                !string.contains('\n'),
+                "Cannot push string with newlines to PrintItemBuffer {string:?}"
+            );
+            assert!(
+                !string.contains('\t'),
+                "Cannot push string with tabs to PrintItemBuffer {string:?}"
+            );
         }
         self.apply_end_request();
         self.items.push_string(string);
     }
 
-    /// Pushes a literal tab character to the buffer.
+    /// Applies trailing requests and pushes a literal tab character to the buffer.
+    ///
     /// Do not use this for indentation, use [`Self::start_indent_before_request`] instead.
     pub fn push_tab(&mut self) {
+        self.apply_end_request();
         self.items.push_signal(Signal::Tab);
     }
 
+    /// Applies trailing requests and pushes a string to the buffer whose content is known at compile time.
+    ///
+    /// To obtain a [`dprint_core::formatting::StringContainer`] use [`dprint_core_macros::sc!`].
+    /// If you need to push a string whose content is not known at compile time, use [`Self::push_string`].
     pub fn push_sc(
         &mut self,
         sc: &'static dprint_core::formatting::StringContainer,
@@ -133,6 +188,10 @@ impl PrintItemBuffer {
         self.items.push_sc(sc);
     }
 
+    /// Inserts a dprint-info into the buffer, before all trailing requests.
+    ///
+    /// This does not apply any trailing request, but instead inserts the info before them.
+    /// If you need to add the info *after* trailing requests, manually call [`Self::apply_end_request`].
     pub fn push_info_before_requests<T>(
         &mut self,
         info: T,
@@ -142,6 +201,10 @@ impl PrintItemBuffer {
         self.items.push_info(info);
     }
 
+    /// Inserts a dprint-anchor into the buffer, before all trailing requests.
+    ///
+    /// This does not apply any trailing request, but instead inserts the anchor before them.
+    /// If you need to add the anchor *after* trailing requests, manually call [`Self::apply_end_request`].
     pub fn push_anchor_before_requests<T>(
         &mut self,
         anchor: T,
@@ -151,6 +214,10 @@ impl PrintItemBuffer {
         self.items.push_anchor(anchor);
     }
 
+    /// Inserts a dprint-condition into the buffer, before all trailing requests.
+    ///
+    /// This does not apply any trailing request, but instead inserts the condition before them.
+    /// If you need to add the condition *after* trailing requests, manually call [`Self::apply_end_request`].
     pub fn push_condition_before_requests(
         &mut self,
         condition: dprint_core::formatting::Condition,
@@ -158,6 +225,10 @@ impl PrintItemBuffer {
         self.items.push_condition(condition);
     }
 
+    /// Inserts a dprint-reevaluation into the buffer, before all trailing requests.
+    ///
+    /// This does not apply any trailing request, but instead inserts the reevaluation before them.
+    /// If you need to add the reevaluation *after* trailing requests, manually call [`Self::apply_end_request`].
     pub fn push_reevaluation_before_requests(
         &mut self,
         reeval: dprint_core::formatting::ConditionReevaluation,
@@ -165,26 +236,58 @@ impl PrintItemBuffer {
         self.items.push_reevaluation(reeval);
     }
 
+    /// Starts a new indent level at the point before all trailing requests.
+    ///
+    /// This does not apply any trailing request, but instead starts the indentation before them.
+    /// If you need to start the indentation *after* trailing requests, manually call [`Self::apply_end_request`].
     pub fn start_indent_before_requests(&mut self) {
         self.items.push_signal(Signal::StartIndent);
     }
 
+    /// Finishes a new indent level at the point before all trailing requests.
+    ///
+    /// This does not apply any trailing request, but instead finishes the indentation before them.
+    /// If you need to finish the indentation *after* trailing requests, manually call [`Self::apply_end_request`].
     pub fn finish_indent_before_requests(&mut self) {
         self.items.push_signal(Signal::FinishIndent);
     }
 
-    pub(crate) fn start_ignoring_indent_before_requests(&mut self) {
+    /// Starts ignoring indentation at the point before all trailing requests.
+    ///
+    /// Any lines following this will not be indented, but instead start at column 0.
+    ///
+    /// This does not apply any trailing request, but instead starts ignoring indentation before them.
+    /// If you need to do so *after* trailing requests, manually call [`Self::apply_end_request`].
+    pub fn start_ignoring_indent_before_requests(&mut self) {
         self.items.push_signal(Signal::StartIgnoringIndent);
     }
 
-    pub(crate) fn finish_ignoring_indent_before_requests(&mut self) {
+    /// Stops ignoring indentation at the point before all trailing requests.
+    ///
+    /// Any lines following this will be indented again.
+    ///
+    /// This does not apply any trailing request, but instead stops ignoring indentation before them.
+    /// If you need to do so *after* trailing requests, manually call [`Self::apply_end_request`].
+    pub fn finish_ignoring_indent_before_requests(&mut self) {
         self.items.push_signal(Signal::FinishIgnoringIndent);
     }
 
-    pub(crate) fn start_new_line_group_before_requests(&mut self) {
+    /// Decreases the precedence of following items getting broken into multiple lines.
+    ///
+    /// Linebreaks can happen at points where a [`Request::or_newline()`] was inserted.
+    ///
+    /// This does not apply any trailing request, but instead starts the newline group before them.
+    /// If you need to do so *after* trailing requests, manually call [`Self::apply_end_request`].
+    pub fn start_new_line_group_before_requests(&mut self) {
         self.items.push_signal(Signal::StartNewLineGroup);
     }
 
+    /// Increases the precedence of following items getting broken into multiple lines.
+    ///
+    /// Linebreaks can happen at points where a [`Request::or_newline()`] was inserted.
+    ///
+    /// This does not apply any trailing request, but instead starts the newline group before them.
+    /// If you need to do so *after* trailing requests, manually call [`Self::apply_end_request`].
     pub fn finish_new_line_group_before_requests(&mut self) {
         self.items.push_signal(Signal::FinishNewLineGroup);
     }
