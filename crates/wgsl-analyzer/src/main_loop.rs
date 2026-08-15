7b5277fb7b7cd800ce9a509ad6c1cc7ba409d965
Mona Mayrhofer <mona.mayrhofer@proton.me>
2026-08-15 16:53:19 +0200

    fix w-a main loop dispatcher

--- crates/wgsl-analyzer/src/main_loop.rs
+++ crates/wgsl-analyzer/src/main_loop.rs
@@ -18,11 +18,12 @@ use lsp_types::{
     DidChangeTextDocumentNotification, DidChangeWatchedFilesNotification,
     DidChangeWorkspaceFoldersNotification, DidCloseTextDocumentNotification,
     DidOpenTextDocumentNotification, DidSaveTextDocumentNotification, DocumentDiagnosticRequest,
-    DocumentFilter, DocumentFormattingRequest, ExitNotification, FoldingRangeRequest,
-    InlayHintRefreshRequest, InlayHintRequest, MessageType, Notification as _, Registration,
-    RegistrationParams, RegistrationRequest, SaveOptions, SemanticTokensRefreshRequest,
-    ShutdownRequest, SignatureHelpRequest, TextDocumentFilter, TextDocumentFilterPattern,
-    TextDocumentRegistrationOptions, TextDocumentSaveRegistrationOptions, Uri,
+    DocumentFilter, DocumentFormattingRequest, DocumentRangeFormattingRequest, ExitNotification,
+    FoldingRangeRequest, InlayHintRefreshRequest, InlayHintRequest, MessageType, Notification as _,
+    Registration, RegistrationParams, RegistrationRequest, SaveOptions,
+    SemanticTokensRefreshRequest, ShutdownRequest, SignatureHelpRequest, TextDocumentFilter,
+    TextDocumentFilterPattern, TextDocumentRegistrationOptions,
+    TextDocumentSaveRegistrationOptions, Uri,
 };
 use project_model::{PackageKey, ProjectManifest};
 use salsa::{Cancelled, Durability};
@@ -955,10 +956,10 @@ impl GlobalState {
         // FIXME: Some of these NO_RETRY could be retries if the file they are interested didn't change.
         // All other request handlers
         dispatcher
-            .on::<NO_RETRY, lt::request::GotoDefinition>(handlers::request::handle_goto_definition)
-            .on::<RETRY, lt::request::Completion>(handlers::request::handle_completion)
-            .on_fmt_thread::<lt::request::Formatting>(handlers::request::handle_formatting)
-            .on_fmt_thread::<lt::request::RangeFormatting>(
+            .on::<NO_RETRY, DefinitionRequest>(handlers::request::handle_goto_definition)
+            .on::<RETRY, CompletionRequest>(handlers::request::handle_completion)
+            .on_fmt_thread::<DocumentFormattingRequest>(handlers::request::handle_formatting)
+            .on_fmt_thread::<DocumentRangeFormattingRequest>(
                 handlers::request::handle_range_formatting,
             )
             .on::<RETRY, lt::request::FoldingRangeRequest>(handlers::request::handle_folding_range)
