4e6b1756a705bb540c7263c84fd7dfe98b03fe2b
Mona Mayrhofer <mona.mayrhofer@proton.me>
2026-09-04 18:46:53 +0200

    dont dedent condcomp attrs without compound statement

---
+++ crates/wgsl_formatter/src/tests/normative/attributes/conditional_compilation.rs
@@ -0,0 +1,98 @@
+use expect_test::expect;
+
+use crate::test_util::check;
+
+#[test]
+fn format_condcomp_attribute_with_compound_gets_merged() {
+    check(
+        "
+        fn main() {
+        @if(true)
+        {
+            var x: u32;
+        }
+        @elif(false)
+        {
+            var x: u32;
+        }
+        @else
+        {
+            var x: u32;
+        }
+            return x;
+        }
+        ",
+        expect![[r#"
+            fn main() {
+            @if(true) {
+                var x: u32;
+            } @elif(false) {
+                var x: u32;
+            } @else {
+                var x: u32;
+            }
+                return x;
+            }
+        "#]],
+    );
+}
+
+#[test]
+fn format_condcomp_attribute_with_compound_gets_detented() {
+    check(
+        "
+        fn main() {
+            @if(true) {
+            var x: u32;
+            } @elif(false) {
+            var x: u32;
+            } @else {
+            var x: u32;
+            }
+            return x;
+        }
+        ",
+        expect![[r#"
+            fn main() {
+            @if(true) {
+                var x: u32;
+            } @elif(false) {
+                var x: u32;
+            } @else {
+                var x: u32;
+            }
+                return x;
+            }
+        "#]],
+    );
+}
+
+#[test]
+fn format_condcomp_attribute_without_compound_gets_indetented() {
+    // Conditional compound statements do not create a scope - and as such should not be indented
+    //https://discord.com/channels/1289346613185351722/1341941812675481680/1537181486279557160
+    check(
+        "
+        fn main() {
+        @if(true)
+        var x: u32;
+        @elif(false)
+        var x: u32;
+        @else
+        var x: u32;
+            return x;
+        }
+        ",
+        expect![[r#"
+            fn main() {
+                @if(true)
+                var x: u32;
+                @elif(false)
+                var x: u32;
+                @else
+                var x: u32;
+                return x;
+            }
+        "#]],
+    );
+}
