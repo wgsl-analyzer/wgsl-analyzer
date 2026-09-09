6ecaba60baced832c515a512007889019ecd7326
Benjamin Brienen <benjamin.brienen@outlook.com>
2026-09-09 23:18:23 +0200

    fmt

--- crates/wgsl-formatter/src/multiline_group.rs
+++ crates/wgsl-formatter/src/multiline_group.rs
@@ -118,7 +118,10 @@ impl<'buffer> MultilineGroup<'buffer> {
             ));
     }
 
-    #[cfg_attr(not(debug_assertions), expect(unused_mut, reason = "state not modified"))]
+    #[cfg_attr(
+        not(debug_assertions),
+        expect(unused_mut, reason = "state not modified")
+    )]
     pub(crate) fn end_before_requests(mut self) {
         #[cfg(debug_assertions)]
         {
