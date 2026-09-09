75b0aed84401ed4f5f8051af5610b492ab6a09f1
Benjamin Brienen <benjamin.brienen@outlook.com>
2026-09-09 18:43:45 +0200

    a

--- crates/wgsl-formatter/src/lib.rs
+++ crates/wgsl-formatter/src/lib.rs
@@ -5,7 +5,7 @@
 //! ## Entrypoints
 //!
 //! The entry points that actually format code are [`format_file`], [`format_range`] [`format_node`] and [`format_tree`].
-#![cfg_attr(doc, doc = include_str!("../Architecture.md"))]
+#![cfg_attr(doc, doc = include_str!("../architecture.md"))]
 #![warn(unused)]
 
 pub mod generators;
